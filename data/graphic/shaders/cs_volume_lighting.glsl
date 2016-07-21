17fa28d5c80e6273311dfef2822e480998cc314d
Tobias Schwandt <t.schwandt@gmail.com>
2016-07-21 15:20:13 +0200

    Add 3D perlin noise by Simon Green

--- data/graphic/shaders/cs_volume_lighting.glsl
+++ data/graphic/shaders/cs_volume_lighting.glsl
@@ -2,11 +2,66 @@
 #define __INCLUDE_CS_VOLUME_LIGHTING_GLSL__
 
 layout (binding = 0, r32f) readonly  uniform image2D cs_InputImage;
-layout (binding = 1, rgba32f) writeonly uniform image3D cs_OutputImage;
+layout (binding = 1, rgba16f) writeonly uniform image3D cs_OutputImage;
+
+layout (binding = 2, rgba16f) readonly uniform image2D cs_PermutationImage;
+layout (binding = 3, rgba16f) readonly uniform image2D cs_PermutationGradientImage;
 
 // -------------------------------------------------------------------------------------
-// Main
+// Simon Green. 
+// “Implementing Improved Perlin Noise.” 
+// In GPU Gems 2, edited by Matt Farr, pp. 409–416, Addison-Wesley Professional, 2005.
 // -------------------------------------------------------------------------------------
+vec3 GetFade(in vec3 t)
+{
+	return t * t * t * (t * (t * 6.0f - 15.0f) + 10.0f); // new curve
+//	return t * t * (3 - 2 * t); // old curve
+}
+
+vec4 SamplePermutation(in vec2 _UV)
+{
+	return imageLoad(cs_PermutationImage, ivec2(_UV * vec2(256.0f)));
+}
+
+float SampleGradientPermutation(in float _U, in vec3 _Point)
+{
+	return dot(imageLoad(cs_PermutationImage, ivec2(_U * 256, 0)).xyz, _Point);
+}
+
+float ImprovedPerlinNoise3D(in vec3 _Seed)
+{
+	// -------------------------------------------------------------------------------------
+	// 1. FIND UNIT CUBE THAT CONTAINS POINT
+	// 2. FIND RELATIVE X,Y,Z OF POINT IN CUBE.
+	// 3. COMPUTE FADE CURVES FOR EACH OF X,Y,Z.
+	// -------------------------------------------------------------------------------------
+	vec3 UV = mod(floor(_Seed), vec3(256.0f));
+  	_Seed -= floor(_Seed);
+
+	vec3 Fade = GetFade(_Seed);
+
+	UV = UV / 256.0f;
+
+	const float One = 1.0f;
+	
+	// -------------------------------------------------------------------------------------
+    // HASH COORDINATES OF THE 8 CUBE CORNERS
+    // -------------------------------------------------------------------------------------
+	vec4 Hash = SamplePermutation(UV.xy) + UV.z;
+ 
+ 	// -------------------------------------------------------------------------------------
+	// AND ADD BLENDED RESULTS FROM 8 CORNERS OF CUBE
+	// -------------------------------------------------------------------------------------
+  	return mix( mix( mix( SampleGradientPermutation(Hash.x, _Seed ),  
+                          SampleGradientPermutation(Hash.z, _Seed + vec3(-1.0f,  0.0f, 0.0f) ), Fade.x),
+                     mix( SampleGradientPermutation(Hash.y, _Seed + vec3( 0.0f, -1.0f, 0.0f) ),
+                          SampleGradientPermutation(Hash.w, _Seed + vec3(-1.0f, -1.0f, 0.0f) ), Fade.x), Fade.y),
+                           
+                mix( mix( SampleGradientPermutation(Hash.x + One, _Seed + vec3( 0.0f,  0.0f, -1.0f) ),
+                          SampleGradientPermutation(Hash.z + One, _Seed + vec3(-1.0f,  0.0f, -1.0f) ), Fade.x),
+                     mix( SampleGradientPermutation(Hash.y + One, _Seed + vec3( 0.0f, -1.0f, -1.0f) ),
+                          SampleGradientPermutation(Hash.w + One, _Seed + vec3(-1.0f, -1.0f, -1.0f) ), Fade.x), Fade.y), Fade.z);
+}
 
 layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
 void main()
@@ -17,7 +72,9 @@ void main()
 
     vec4 Pixel = imageLoad(cs_InputImage, ivec2(X, Y));
 
-    imageStore(cs_OutputImage, ivec3(X, Y, Z), vec4(1,0,0,0));
+    float PerlinNoise = ImprovedPerlinNoise3D(vec3(X / 160.0f, Y / 90.0f, Z / 128.0f));
+
+    imageStore(cs_OutputImage, ivec3(X, Y, Z), vec4(PerlinNoise * 100.0f, 0, 0, 0));
 }
 
 #endif // __INCLUDE_CS_VOLUME_LIGHTING_GLSL__
--- intern/src/graphic/gfx_fog_renderer.cpp
+++ intern/src/graphic/gfx_fog_renderer.cpp
@@ -94,6 +94,9 @@ namespace
         CTexture3DPtr     m_VolumeTexturePtr;
         CTextureSetPtr    m_VolumeTextureSetPtr;
 
+        CTexture2DPtr m_PermutationTexturePtr;
+        CTexture2DPtr m_GradientPermutationTexturePtr;
+
         CShaderPtr     m_GaussianBlurShaderPtr;
         CBufferSetPtr  m_GaussianBlurPropertiesCSBufferSetPtr;
         CTextureSetPtr m_BlurStagesTextureSetPtrs[2];
@@ -124,6 +127,8 @@ namespace
         , m_ESMTextureSetPtr                    ()
         , m_VolumeTexturePtr                    ()
         , m_VolumeTextureSetPtr                 ()
+        , m_PermutationTexturePtr               ()
+        , m_GradientPermutationTexturePtr       ()
         , m_GaussianBlurShaderPtr               ()
         , m_GaussianBlurPropertiesCSBufferSetPtr()
         , m_BlurStagesTextureSetPtrs            ()
@@ -147,20 +152,22 @@ namespace
     
     void CGfxFogRenderer::OnExit()
     {
-        m_QuadModelPtr            = 0;
-        m_FullQuadViewVSBufferPtr = 0;
-        m_P2InputLayoutPtr        = 0;
-        m_RectangleShaderVSPtr    = 0;
-        m_ESMCSPtr                = 0;
-        m_VolumeLightingCSPtr     = 0;
-        m_VolumeScatteringCSPtr   = 0;
-        m_ApplyPSPtr              = 0;
-        m_PSSamplerSetPtr         = 0;
-        m_LightRenderContextPtr   = 0;
-        m_ESMTexturePtr           = 0;
-        m_ESMTextureSetPtr        = 0;
-        m_VolumeTexturePtr        = 0;
-        m_VolumeTextureSetPtr     = 0;
+        m_QuadModelPtr                  = 0;
+        m_FullQuadViewVSBufferPtr       = 0;
+        m_P2InputLayoutPtr              = 0;
+        m_RectangleShaderVSPtr          = 0;
+        m_ESMCSPtr                      = 0;
+        m_VolumeLightingCSPtr           = 0;
+        m_VolumeScatteringCSPtr         = 0;
+        m_ApplyPSPtr                    = 0;
+        m_PSSamplerSetPtr               = 0;
+        m_LightRenderContextPtr         = 0;
+        m_ESMTexturePtr                 = 0;
+        m_ESMTextureSetPtr              = 0;
+        m_VolumeTexturePtr              = 0;
+        m_VolumeTextureSetPtr           = 0;
+        m_PermutationTexturePtr         = 0;
+        m_GradientPermutationTexturePtr = 0;
 
         m_GaussianBlurShaderPtr                = 0;
         m_GaussianBlurPropertiesCSBufferSetPtr = 0;
@@ -258,6 +265,121 @@ namespace
 
         // -----------------------------------------------------------------------------
 
+        static int s_PerlinPermutations[] = 
+        { 
+            151,160,137,91,90,15,
+            131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
+            190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+            88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+            77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+            102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+            135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+            5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+            223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+            129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+            251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+            49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+            138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
+        };
+
+        auto GetPermutationAtIndex = [&](int _Index) -> int
+        {
+            return s_PerlinPermutations[_Index % 256];
+        };
+
+        auto GetPermutationAtPosition = [&](const Base::UInt2& _rUV) -> Base::Float4
+        {
+            int A  = GetPermutationAtIndex(_rUV[0]) + _rUV[1];
+            int AA = GetPermutationAtIndex(A);
+            int AB = GetPermutationAtIndex(A + 1);
+            int B  = GetPermutationAtIndex(_rUV[0] + 1) + _rUV[1];
+            int BA = GetPermutationAtIndex(B);
+            int BB = GetPermutationAtIndex(B + 1);
+
+            return Base::Float4(static_cast<float>(AA), static_cast<float>(AB), static_cast<float>(BA), static_cast<float>(BB)) / 255.0f;
+        };
+
+
+        Base::Float4* pPermutationData = static_cast<Base::Float4*>(Base::CMemory::Allocate(sizeof(Base::Float4) * 256 * 256));
+
+        for (unsigned int Y = 0; Y < 256; ++Y)
+        {
+            for (unsigned int X = 0; X < 256; ++X)
+            {
+                pPermutationData[Y * 256 + X] = GetPermutationAtPosition(Base::UInt2(X, Y));
+            }
+        }
+
+        RendertargetDescriptor.m_NumberOfPixelsU  = 256;
+        RendertargetDescriptor.m_NumberOfPixelsV  = 256;
+        RendertargetDescriptor.m_NumberOfPixelsW  = 1;
+        RendertargetDescriptor.m_NumberOfMipMaps  = 1;
+        RendertargetDescriptor.m_NumberOfTextures = 1;
+        RendertargetDescriptor.m_Binding          = CTextureBase::ShaderResource;
+        RendertargetDescriptor.m_Access           = CTextureBase::CPUWrite;
+        RendertargetDescriptor.m_Format           = CTextureBase::R16G16B16A16_FLOAT;
+        RendertargetDescriptor.m_Usage            = CTextureBase::GPUReadWrite;
+        RendertargetDescriptor.m_Semantic         = CTextureBase::Diffuse;
+        RendertargetDescriptor.m_pFileName        = 0;
+        RendertargetDescriptor.m_pPixels          = pPermutationData;
+
+        m_PermutationTexturePtr = TextureManager::CreateTexture2D(RendertargetDescriptor);
+
+        Base::CMemory::Free(pPermutationData);
+
+        // -----------------------------------------------------------------------------
+
+        static Base::Float4 s_PerlinGradients[] = 
+        {
+            Base::Float4(1,1,0,     0),
+            Base::Float4(-1,1,0,    0),
+            Base::Float4(1,-1,0,    0),
+            Base::Float4(-1,-1,0,   0),
+            Base::Float4(1,0,1,     0),
+            Base::Float4(-1,0,1,    0),
+            Base::Float4(1,0,-1,    0),
+            Base::Float4(-1,0,-1,   0),
+            Base::Float4(0,1,1,     0),
+            Base::Float4(0,-1,1,    0),
+            Base::Float4(0,1,-1,    0),
+            Base::Float4(0,-1,-1,   0),
+            Base::Float4(1,1,0,     0),
+            Base::Float4(0,-1,1,    0),
+            Base::Float4(-1,1,0,    0),
+            Base::Float4(0,-1,-1,   0),
+        };
+
+        auto GetPermutationGradientAtIndex = [&](unsigned int _Index) -> Base::Float4
+        {
+            return s_PerlinGradients[s_PerlinPermutations[_Index] % 16];
+        };
+
+        Base::Float4* pPermutationGradientData = static_cast<Base::Float4*>(Base::CMemory::Allocate(sizeof(Base::Float4) * 256));
+
+        for (unsigned int X = 0; X < 256; ++X)
+        {
+            pPermutationGradientData[X] = GetPermutationGradientAtIndex(X);
+        }
+
+        RendertargetDescriptor.m_NumberOfPixelsU  = 256;
+        RendertargetDescriptor.m_NumberOfPixelsV  = 1;
+        RendertargetDescriptor.m_NumberOfPixelsW  = 1;
+        RendertargetDescriptor.m_NumberOfMipMaps  = 1;
+        RendertargetDescriptor.m_NumberOfTextures = 1;
+        RendertargetDescriptor.m_Binding          = CTextureBase::ShaderResource;
+        RendertargetDescriptor.m_Access           = CTextureBase::CPUWrite;
+        RendertargetDescriptor.m_Format           = CTextureBase::R16G16B16A16_FLOAT;
+        RendertargetDescriptor.m_Usage            = CTextureBase::GPUReadWrite;
+        RendertargetDescriptor.m_Semantic         = CTextureBase::Diffuse;
+        RendertargetDescriptor.m_pFileName        = 0;
+        RendertargetDescriptor.m_pPixels          = pPermutationGradientData;
+
+        m_GradientPermutationTexturePtr = TextureManager::CreateTexture2D(RendertargetDescriptor);
+
+        Base::CMemory::Free(pPermutationGradientData);
+
+        // -----------------------------------------------------------------------------
+
         RendertargetDescriptor.m_NumberOfPixelsU  = 160;
         RendertargetDescriptor.m_NumberOfPixelsV  = 90;
         RendertargetDescriptor.m_NumberOfPixelsW  = 128;
@@ -265,7 +387,7 @@ namespace
         RendertargetDescriptor.m_NumberOfTextures = 1;
         RendertargetDescriptor.m_Binding          = CTextureBase::ShaderResource;
         RendertargetDescriptor.m_Access           = CTextureBase::CPUWrite;
-        RendertargetDescriptor.m_Format           = CTextureBase::R32G32B32A32_FLOAT;
+        RendertargetDescriptor.m_Format           = CTextureBase::R16G16B16A16_FLOAT;
         RendertargetDescriptor.m_Usage            = CTextureBase::GPUReadWrite;
         RendertargetDescriptor.m_Semantic         = CTextureBase::Diffuse;
         RendertargetDescriptor.m_pFileName        = 0;
@@ -273,7 +395,7 @@ namespace
 
         m_VolumeTexturePtr = TextureManager::CreateTexture3D(RendertargetDescriptor);
 
-        m_VolumeTextureSetPtr = TextureManager::CreateTextureSet(static_cast<CTextureBasePtr>(m_ESMTexturePtr), static_cast<CTextureBasePtr>(m_VolumeTexturePtr));
+        m_VolumeTextureSetPtr = TextureManager::CreateTextureSet(static_cast<CTextureBasePtr>(m_ESMTexturePtr), static_cast<CTextureBasePtr>(m_VolumeTexturePtr), static_cast<CTextureBasePtr>(m_PermutationTexturePtr), static_cast<CTextureBasePtr>(m_GradientPermutationTexturePtr));
 
         // -----------------------------------------------------------------------------
 
