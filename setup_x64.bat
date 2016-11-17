dfff7614bb13d395826855b9e8d0d94708121735
Tobias Schwandt <t.schwandt@gmail.com>
2016-11-17 20:00:43 +0100

    Add batch file to download and install dependencies

---
+++ setup_x64.bat
@@ -0,0 +1,66 @@
+@echo "= Welcome to Saltwater"
+@echo "This script download and installs all dependencies for running the application"
+@echo "Mode: x64"
+
+@echo off
+set BuildMode=x64
+@echo on
+
+@echo "Start downloading dependencies..."
+
+@echo off
+if not exist "tmp_dependencies.zip" curl http://data.tobias-schwandt.de/dependencies_win32_x64.zip --output tmp_dependencies.zip
+@echo on
+
+@echo "Downloading dependencies finished"
+
+
+
+@echo "Start unzipping dependencies..."
+
+@echo off
+Call :UnZipFile "%cd%\" "%cd%\tmp_dependencies.zip"
+
+del tmp_dependencies.zip
+@echo on
+
+@echo "Unzipping dependencies finished"
+
+
+
+@echo "Copy dynamic dependencies..."
+
+@echo off
+if not exist "bin" mkdir bin
+
+copy extern\artoolkit\bin\%BuildMode%\*.dll bin\
+copy extern\assimp\bin\%BuildMode%\*.dll bin\
+copy extern\devil\bin\%BuildMode%\*.dll bin\
+copy extern\glew\bin\%BuildMode%\*.dll bin\
+copy extern\opencv\bin\%BuildMode%\*.dll bin\
+copy extern\qt\bin\%BuildMode%\*.dll bin\
+copy extern\sdl2\bin\%BuildMode%\*.dll bin\
+copy extern\tinyxml2\bin\%BuildMode%\*.dll bin\
+@echo on
+
+@echo "Copying finished"
+
+
+
+@echo "All dependencies are installed. Please follow the instructions in README.md"
+
+
+:UnZipFile <ExtractTo> <newzipfile>
+set vbs="%temp%\_.vbs"
+if exist %vbs% del /f /q %vbs%
+>%vbs%  echo Set fso = CreateObject("Scripting.FileSystemObject")
+>>%vbs% echo If NOT fso.FolderExists(%1) Then
+>>%vbs% echo fso.CreateFolder(%1)
+>>%vbs% echo End If
+>>%vbs% echo set objShell = CreateObject("Shell.Application")
+>>%vbs% echo set FilesInZip=objShell.NameSpace(%2).items
+>>%vbs% echo objShell.NameSpace(%1).CopyHere(FilesInZip)
+>>%vbs% echo Set fso = Nothing
+>>%vbs% echo Set objShell = Nothing
+cscript //nologo %vbs%
+if exist %vbs% del /f /q %vbs%
