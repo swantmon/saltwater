78a81c247a4616f714af2d643d31e3dd3505a4bb
Unknown <kuno1234@gmx.de>
2017-05-08 16:40:45 +0200

    Apply sobel filter to depth image

--- data/graphic/shaders/kinect_fusion/cs_contour_normals.glsl
+++ data/graphic/shaders/kinect_fusion/cs_contour_normals.glsl
@@ -19,9 +19,32 @@ layout(binding = 1, MAP_TEXTURE_FORMAT) uniform image2D cs_NormalBuffer;
 // Functions
 // -------------------------------------------------------------------------------------
 
+const int g_KernelSize = 5;
+
+float g_SobelKernel[g_KernelSize * g_KernelSize] = {
+    -0.25f, -0.20f, 0.0f, 0.20f, 0.25f,
+    -0.20f, -0.50f, 0.0f, 0.50f, 0.20f,
+    -0.50f, -1.00f, 0.0f, 1.00f, 0.50f,
+    -0.20f, -0.50f, 0.0f, 0.50f, 0.20f,
+    -0.25f, -0.20f, 0.0f, 0.20f, 0.25f,
+};
+
 vec2 ComputeGradient(ivec2 Position)
 {
-    
+    vec2 Result = vec2(0.0f);
+
+    for (int i = 0; i < g_KernelSize; ++i)
+    {
+        for (int j = 0; j < g_KernelSize; ++j)
+        {
+            float Sample = float(imageLoad(cs_DepthBuffer, Position + ivec2(i, j)));
+
+            Result.x += Sample * g_SobelKernel[i * g_KernelSize + j];
+            Result.y += Sample * g_SobelKernel[j * g_KernelSize + i];
+        }
+    }
+
+    return Result;
 }
 
 layout (local_size_x = TILE_SIZE2D, local_size_y = TILE_SIZE2D, local_size_z = 1) in;
@@ -30,7 +53,7 @@ void main()
     const int x = int(gl_GlobalInvocationID.x);
     const int y = int(gl_GlobalInvocationID.y);
     
-    imageStore(cs_NormalBuffer, ivec2(x, y), vec4(ComputeGradient(ivec2(x, y)), 0.5, 1.0));
+    imageStore(cs_NormalBuffer, ivec2(x, y), vec4(ComputeGradient(ivec2(x, y)), 0.0, 1.0));
 }
 
 #endif // __INCLUDE_CS_CONTOURS_NORMAL_GLSL__
