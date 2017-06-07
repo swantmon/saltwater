011550daf9dd1b28488cd37dfde998174aaac548
Unknown <kuno1234@gmx.de>
2017-06-07 12:25:22 +0200

    Make volume resizable

--- data/graphic/shaders/kinect_fusion/vs_raycast.glsl
+++ data/graphic/shaders/kinect_fusion/vs_raycast.glsl
@@ -27,9 +27,11 @@ out gl_PerVertex
 
 void main()
 {
-    out_WSRayDirection = in_VertexPosition.xyz - g_ViewPosition.xyz;
+	const vec3 WSPosition = in_VertexPosition * VOLUME_SIZE;
 
-    gl_Position = g_WorldToScreen * vec4(in_VertexPosition, 1.0f);
+    out_WSRayDirection = WSPosition - g_ViewPosition.xyz;
+
+    gl_Position = g_WorldToScreen * vec4(WSPosition, 1.0f);
 }
 
 #endif // __INCLUDE_VS_KINECT_RAYCAST_GLSL__
