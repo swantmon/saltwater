ac320517900fd638597721d977a7bd7d8b115acc
Unknown <kuno1234@gmx.de>
2017-08-10 14:36:31 +0200

    Use inverse pose matrix

--- data/graphic/shaders/scalable_kinect_fusion/vs_rasterize_rootvolume.glsl
+++ data/graphic/shaders/scalable_kinect_fusion/vs_rasterize_rootvolume.glsl
@@ -46,12 +46,12 @@ void main()
     out_Index = InstanceData.m_Index;
     vec3 Offset = InstanceData.m_Offset;
 
-	vec4 Vertex = (g_PoseMatrix * vec4((in_VertexPosition + Offset) * VOLUME_SIZE, 1.0f));
+	vec4 Vertex = (g_InvPoseMatrix * vec4((in_VertexPosition + Offset) * VOLUME_SIZE, 1.0f));
     
 	Vertex.xy = Vertex.xy * g_Intrinsics[0].m_FocalLength / Vertex.z + g_Intrinsics[0].m_FocalPoint;
 	Vertex.xy = Vertex.xy / vec2(DEPTH_IMAGE_WIDTH, DEPTH_IMAGE_HEIGHT) * 2.0f - 1.0f;
 	Vertex.z = ((Vertex.z - 0.5f) / 7.5f) * 2.0f - 1.0f;
-    
+
     gl_Position = Vertex;
 }
 
