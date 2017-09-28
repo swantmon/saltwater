22a5fbb455d42f82f6b8d1735fa4b7139e39e35f
Unknown <kuno1234@gmx.de>
2017-09-28 16:30:20 +0200

    Check if shader invocation is in queue

--- data/graphic/shaders/scalable_kinect_fusion/integration/cs_integrate_rootgrid.glsl
+++ data/graphic/shaders/scalable_kinect_fusion/integration/cs_integrate_rootgrid.glsl
@@ -4,21 +4,19 @@
 
 #include "scalable_kinect_fusion/common_scalable.glsl"
 
-// -----------------------------------------------------------------------------
-// Constants
-// -----------------------------------------------------------------------------
-
-// -----------------------------------------------------------------------------
-// Input from engine
-// -----------------------------------------------------------------------------
-
-// -------------------------------------------------------------------------------------
-// Functions
-// -------------------------------------------------------------------------------------
+layout(std430, binding = 6) buffer Level1Queue
+{
+    uint g_VolumeID[];
+};
 
 layout (local_size_x = TILE_SIZE1D, local_size_y = 1, local_size_z = 1) in;
 void main()
 {
+    if (gl_GlobalInvocationID.x < g_Level1QueueSize)
+    {
+        uint VoxelIndex = g_VolumeID[gl_GlobalInvocationID.x];
+    }
+
     
 }
 
