64419c5d1894b3fc51f169d4fe9267379988733a
Unknown <kuno1234@gmx.de>
2017-10-09 17:02:18 +0200

    Use compute shader to check voxels

--- data/graphic/shaders/scalable_kinect_fusion/rasterization_reverse/fs_rootgrid.glsl
+++ data/graphic/shaders/scalable_kinect_fusion/rasterization_reverse/fs_rootgrid.glsl
@@ -6,7 +6,7 @@ layout(location = 0) out vec4 out_Color;
 
 void main()
 {
-    out_Color = vec4(0.0f, 1.0f, 0.0f, 1.0f);
+    out_Color = vec4(1.0f);
 }
 
 #endif // __INCLUDE_FS_RASTERIZATION_ROOTGRID_GLSL__
--- data/graphic/shaders/scalable_kinect_fusion/rasterization_reverse/gs_rootgrid.glsl
+++ data/graphic/shaders/scalable_kinect_fusion/rasterization_reverse/gs_rootgrid.glsl
@@ -12,7 +12,7 @@ layout(row_major, std140, binding = 0) uniform PerVolumeData
     int g_Resolution;
 };
 
-layout (binding = 0, rgba16f) uniform image2D cs_VertexMap;
+layout(binding = 0, rgba16f) uniform image2D cs_VertexMap;
 
 // -----------------------------------------------------------------------------
 // Inputs
@@ -52,7 +52,7 @@ void main()
     vec3 VSPosition = WSPosition - g_Offset * VOLUME_SIZE;
     VSPosition = (VSPosition / VOLUME_SIZE) * 2.0f - 1.0f;
     VSPosition.z = VSPosition.z * 0.5f + 0.5f;
-
+    VSPosition.z = 0.5f;
     gl_Layer = 0;
 
     gl_Position = vec4(VSPosition, 1.0f);
