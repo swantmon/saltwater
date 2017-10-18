f82295f7afb580e9f8aa38fa9f569ff5110d6353
Unknown <kuno1234@gmx.de>
2017-10-18 15:37:28 +0200

    Choose slam reconstructor with config value

--- intern/src/mr/mr_slam_reconstruction_settings.cpp
+++ intern/src/mr/mr_slam_reconstruction_settings.cpp
@@ -1,6 +1,8 @@
 
 #include "mr/mr_precompiled.h"
 
+#include "base/base_program_parameters.h"
+
 #include "mr_slam_reconstruction_settings.h"
 
 namespace MR
@@ -14,10 +16,12 @@ namespace MR
         , m_PyramidLevelCount(3)
         , m_DepthThreshold(500, 8000)
         , m_CaptureColor(false)
-		, m_IsScalable(true)
+		, m_IsScalable(false)
         , m_UseFullVolumeIntegration(true)
         , m_UseReverseIntegration(true)
     {
+        m_IsScalable = Base::CProgramParameters::GetInstance().GetBoolean("scalable_slam");
+
         m_PyramidLevelIterations[0] = 10;
         m_PyramidLevelIterations[1] = 5;
         m_PyramidLevelIterations[2] = 4;
