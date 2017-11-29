8bd89d3a805aabe45144db435755c5ec05df0b03
Tobias Schwandt <t.schwandt@gmail.com>
2017-11-29 15:25:18 +0100

    Add new droid test project

---
+++ intern/project/droid3/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,14 @@
+#include <jni.h>
+#include <string>
+
+extern "C"
+JNIEXPORT jstring
+
+JNICALL
+Java_de_tu_1ilmenau_saltwater_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
