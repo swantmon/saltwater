cb7b4a20b190b0c818f803a12cc238c7fe67e5c5
Tobias Schwandt <t.schwandt@gmail.com>
2017-11-30 09:53:09 +0100

    Test new project config

--- intern/project/droid3/app/src/main/cpp/native-lib.cpp
+++ intern/project/droid3/app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,8 @@
 #include <jni.h>
 #include <string>
 
+#include "core/core_time.h"
+
 extern "C"
 JNIEXPORT jstring
 
@@ -8,6 +10,14 @@ JNICALL
 Java_de_tu_1ilmenau_saltwater_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
+
+    Core::Time::Update();
+    Core::Time::Update();
+    Core::Time::Update();
+
+    int Frame = Core::Time::GetNumberOfFrame();
+
+    double D = Core::Time::GetDeltaTimeLastFrame();
 
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
