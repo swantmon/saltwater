42d9203fcde948217714feb10ab9910172c7b6ba
Tobias Schwandt <t.schwandt@gmail.com>
2018-01-08 14:02:25 +0100

    Remove ARToolkit

--- intern/src/mr/mr_control_manager.h
+++ intern/src/mr/mr_control_manager.h
@@ -1,22 +1,12 @@
 
 #pragma once
 
-#include "mr/mr_control.h"
-
 namespace MR
 {
 namespace ControlManager
 {
     void OnStart();
     void OnExit();
-
-    void Clear();
-
     void Update();
-
-    bool IsActive();
-    bool IsOriginTracked();
-
-    CControl* GetActiveControl();
 } // namespace ControlManager
 } // namespace MR
