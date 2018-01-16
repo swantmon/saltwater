afbfd9cd0d8a39c0b1307f14d6c5ad1712c4be03
Tobias Schwandt <t.schwandt@gmail.com>
2018-01-16 10:16:06 +0100

    Add camera permission functionality

--- intern/project/droid/app/src/main/java/de/tu_ilmenau/saltwater/GameActivity.java
+++ intern/project/droid/app/src/main/java/de/tu_ilmenau/saltwater/GameActivity.java
@@ -4,8 +4,18 @@ package de.tu_ilmenau.saltwater;
 import android.app.NativeActivity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+import android.Manifest;
+
+import android.net.Uri;
+
+import android.provider.Settings;
 
 import android.os.Bundle;
+
+import android.widget.Toast;
 
 public class GameActivity extends NativeActivity
 {
@@ -49,6 +59,13 @@ public class GameActivity extends NativeActivity
     public void onResume()
     {
         super.onResume();
+
+        if (!HasCameraPermission())
+        {
+            RequestCameraPermission();
+
+            return;
+        }
     }
 
     @Override
@@ -67,6 +84,50 @@ public class GameActivity extends NativeActivity
     public void onDestroy()
     {
         super.onDestroy();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int _RequestCode, String[] _Permissions, int[] _Results)
+    {
+        if (!HasCameraPermission())
+        {
+            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG).show();
+
+            if (!ShouldShowRequestPermissionRationale())
+            {
+                // Permission denied with checking "Do not ask again".
+                LaunchPermissionSettings();
+            }
+
+            finish();
+        }
+    }
+
+    // -----------------------------------------------------------------------------
+    // Camera permissions
+    // -----------------------------------------------------------------------------
+    public boolean HasCameraPermission()
+    {
+        return this.checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public void RequestCameraPermission()
+    {
+        this.requestPermissions(new String[] {Manifest.permission.CAMERA}, 0);
+    }
+
+    public boolean ShouldShowRequestPermissionRationale()
+    {
+        return this.shouldShowRequestPermissionRationale(Manifest.permission.CAMERA);
+    }
+
+    public void LaunchPermissionSettings()
+    {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setData(Uri.fromParts("package", this.getPackageName(), null));
+
+        this.startActivity(intent);
     }
 
     // -----------------------------------------------------------------------------
