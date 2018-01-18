d32984de2ba64d00e387c68c704e8ac7d1cddd94
Tobias Schwandt <t.schwandt@gmail.com>
2018-01-18 08:24:03 +0100

    Change background rendering shader for testing

--- data/graphic/shaders/fs_atmosphere_texture.glsl
+++ data/graphic/shaders/fs_atmosphere_texture.glsl
@@ -42,10 +42,8 @@ layout(location = 0) out vec4 out_Output;
 // -----------------------------------------------------------------------------
 void main(void)
 {
-    // -----------------------------------------------------------------------------
-    // TexCoord for kinect textures
-    // -----------------------------------------------------------------------------
-    vec2 UV = vec2(in_UV.x, 1.0f - in_UV.y);
+    out_Output = texture(ps_EnvironmentTexture, in_UV); return;
+
 
     // -----------------------------------------------------------------------------
     // Check Depth
@@ -65,7 +63,7 @@ void main(void)
     // -----------------------------------------------------------------------------
     // Lighting only if depth is infinity
     // -----------------------------------------------------------------------------
-    vec4 FinalColor = texture(ps_EnvironmentTexture, UV);
+    vec4 FinalColor = texture(ps_EnvironmentTexture, in_UV);
 
     if (m_IsHDR == 0.0f)
     {
