65b8c07c2228a64bfb011e1d2819dc58e7c41d6b
Unknown <kuno1234@gmx.de>
2018-01-22 12:19:53 +0100

    Calculate histogram bins

--- data/graphic/shaders/scalable_kinect_fusion/cs_plane_detection.glsl
+++ data/graphic/shaders/scalable_kinect_fusion/cs_plane_detection.glsl
@@ -4,13 +4,19 @@
 
 layout(row_major, std140, binding = 0) uniform HistogramSizes
 {
-    ivec4 g_HistogramSizes;
+    int g_AzimuthBinCount;
+    int g_InclinationBinCount;
+    int g_Unused0;
+    int g_Unused1;
 };
 
 layout(std430, binding = 0) buffer Histogram
 {
     int g_Histogram[];
 };
+
+const float g_Tau = 6.28318530718f;
+const float g_Pi = 6.28318530718f * 0.5f;
 
 // -----------------------------------------------------------------------------
 // Input from engine
@@ -25,17 +31,21 @@ layout (binding = 1, MAP_TEXTURE_FORMAT) uniform image2D cs_NormalMap;
 layout (local_size_x = TILE_SIZE2D, local_size_y = TILE_SIZE2D, local_size_z = 1) in;
 void main()
 {
-	const ivec2 ImageSize = imageSize(cs_VertexMap);
-	
-	const int x = int(gl_GlobalInvocationID.x);
-	const int y = int(gl_GlobalInvocationID.y);
-	
-	vec3 Normal = imageLoad(cs_NormalMap, ivec2(x, y)).xyz;
-	
-	float Inclination = acos(Normal.z);
-	float Azimuth = atan(Normal.y, Normal.x);
-	
-	g_Histogram[gl_LocalInvocationIndex] = int(gl_LocalInvocationIndex);
+    const ivec2 ImageSize = imageSize(cs_VertexMap);
+    
+    const int x = int(gl_GlobalInvocationID.x);
+    const int y = int(gl_GlobalInvocationID.y);
+    
+    vec3 Normal = imageLoad(cs_NormalMap, ivec2(x, y)).xyz;
+    
+    float Azimuth = atan(Normal.y, Normal.x);
+    float Inclination = acos(Normal.z);
+        
+    int HistogramBinX = int((Azimuth / g_Tau + 0.5f) * g_AzimuthBinCount);
+    int HistogramBinY = int(Inclination / g_Pi * g_InclinationBinCount);
+    
+    imageStore(cs_VertexMap, ivec2(x, y), vec4(HistogramBinY, 0, 0, 0));
+    imageStore(cs_NormalMap, ivec2(x, y), vec4(Inclination, 0, 0, 0));
 }
 
 #endif // __INCLUDE_CS_PLANE_DETECTION_GLSL__
