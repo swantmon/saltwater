1e3277306ca24cb1ef31ea72db6d68c32a0cccde
Unknown <kuno1234@gmx.de>
2018-01-23 15:41:41 +0100

    Change parameter names

--- intern/src/graphic/gfx_main.cpp
+++ intern/src/graphic/gfx_main.cpp
@@ -230,17 +230,17 @@ namespace
         else BASE_THROWV("Graphics API %s is not supported! Possible options are \"gles\" or \"gl\"", GraphicsAPI.c_str());
 
 #ifdef __ANDROID__
-        m_GraphicsInfo.m_MajorVersion = Base::CProgramParameters::GetInstance().GetInt("graphics:api:major:version", 3);
-        m_GraphicsInfo.m_MinorVersion = Base::CProgramParameters::GetInstance().GetInt("graphics:api:minor:version", 2);
+        m_GraphicsInfo.m_MajorVersion = Base::CProgramParameters::GetInstance().GetInt("graphics:api:major_version", 3);
+        m_GraphicsInfo.m_MinorVersion = Base::CProgramParameters::GetInstance().GetInt("graphics:api:minor_version", 2);
 #else
-        m_GraphicsInfo.m_MajorVersion = Base::CProgramParameters::GetInstance().GetInt("graphics:api:major:version", 4);
-        m_GraphicsInfo.m_MinorVersion = Base::CProgramParameters::GetInstance().GetInt("graphics:api:minor:version", 5);
+        m_GraphicsInfo.m_MajorVersion = Base::CProgramParameters::GetInstance().GetInt("graphics:api:major_version", 4);
+        m_GraphicsInfo.m_MinorVersion = Base::CProgramParameters::GetInstance().GetInt("graphics:api:minor_version", 5);
 #endif
 
         // -----------------------------------------------------------------------------
         // Load pixel matching behavior
         // -----------------------------------------------------------------------------
-        m_GraphicsInfo.m_PixelMatching = static_cast<CInternGraphicsInfo::EPixelMatching>(Base::CProgramParameters::GetInstance().GetInt("graphics:pixel:matching", 0));
+        m_GraphicsInfo.m_PixelMatching = static_cast<CInternGraphicsInfo::EPixelMatching>(Base::CProgramParameters::GetInstance().GetInt("graphics:pixel_matching:enable", 0));
     }
     
     // -----------------------------------------------------------------------------
@@ -947,7 +947,7 @@ namespace
         {
         case CInternGraphicsInfo::Scale:
         {
-            float Scale = Base::CProgramParameters::GetInstance().GetFloat("graphics:pixel:matching:scale", 1.0f);
+            float Scale = Base::CProgramParameters::GetInstance().GetFloat("graphics:pixel_matching:scale", 1.0f);
 
             _pWindowInfo->m_InternalWindowSize[0] = static_cast<int>(static_cast<float>(_pWindowInfo->m_NativeWindowSize[0]) * Scale);
             _pWindowInfo->m_InternalWindowSize[1] = static_cast<int>(static_cast<float>(_pWindowInfo->m_NativeWindowSize[1]) * Scale);
@@ -955,8 +955,8 @@ namespace
         break;
         case CInternGraphicsInfo::Fix:
         {
-            _pWindowInfo->m_InternalWindowSize[0] = Base::CProgramParameters::GetInstance().GetUInt("graphics:pixel:matching:w", _Width);
-            _pWindowInfo->m_InternalWindowSize[1] = Base::CProgramParameters::GetInstance().GetUInt("graphics:pixel:matching:h", _Height);
+            _pWindowInfo->m_InternalWindowSize[0] = Base::CProgramParameters::GetInstance().GetUInt("graphics:pixel_matching:w", _Width);
+            _pWindowInfo->m_InternalWindowSize[1] = Base::CProgramParameters::GetInstance().GetUInt("graphics:pixel_matching:h", _Height);
         }
         break;
         };
