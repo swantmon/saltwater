e73f093807c8857f9652fbc69d2d11d23548c7fd
Tobias Schwandt <t.schwandt@gmail.com>
2018-01-25 23:01:33 +0100

    Hide navigation bar

--- intern/project/droid/app/src/main/java/de/tuilmenau/saltwater/GameActivity.java
+++ intern/project/droid/app/src/main/java/de/tuilmenau/saltwater/GameActivity.java
@@ -9,6 +9,7 @@ import android.graphics.Point;
 
 import android.os.Bundle;
 
+import android.view.View;
 import android.view.WindowManager;
 
 public class GameActivity extends NativeActivity
@@ -35,10 +36,13 @@ public class GameActivity extends NativeActivity
 
         s_Instance = this;
 
-        int UIOptions = WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-                      | WindowManager.LayoutParams.FLAG_FULLSCREEN;
-
-        getWindow().addFlags(UIOptions);
+        getWindow().getDecorView().setSystemUiVisibility(
+                  View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE);  // NOT sticky.. will be set to sticky later!
 
         nativeInitializeInterface(getApplicationContext());
     }
@@ -57,6 +61,26 @@ public class GameActivity extends NativeActivity
     public void onResume()
     {
         super.onResume();
+
+        RestoreTransparentBars();
+
+        getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener()
+        {
+            @Override
+            public void onSystemUiVisibilityChange(int _Visibility)
+            {
+                RestoreTransparentBars();
+            }
+        });
+
+        getWindow().getDecorView().setOnFocusChangeListener(new View.OnFocusChangeListener()
+        {
+            @Override
+            public void onFocusChange(View _View, boolean _HasFocus)
+            {
+                RestoreTransparentBars();
+            }
+        });
     }
 
     // -----------------------------------------------------------------------------
@@ -119,6 +143,18 @@ public class GameActivity extends NativeActivity
 
         return Size.y;
     }
+
+    public void RestoreTransparentBars()
+    {
+        getWindow().getDecorView().setSystemUiVisibility(
+                  View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
 
     // -----------------------------------------------------------------------------
     // Native functions
