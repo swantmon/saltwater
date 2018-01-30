3b5016c7f54dd75788435ad2ad393266ddc4a4c5
Unknown <kuno1234@gmx.de>
2018-01-30 13:14:19 +0100

    Fix work group size

--- data/graphic/shaders/scalable_kinect_fusion/plane_detection/cs_plane_extraction.glsl
+++ data/graphic/shaders/scalable_kinect_fusion/plane_detection/cs_plane_extraction.glsl
@@ -28,13 +28,20 @@ layout (binding = 2, MAP_TEXTURE_FORMAT) uniform image2D cs_NormalMap;
 // Functions
 // -------------------------------------------------------------------------------------
 
-layout (local_size_x = TILE_SIZE2D, local_size_y = TILE_SIZE2D, local_size_z = 1) in;
+layout (local_size_x = TILE_SIZE2D, local_size_y = 1, local_size_z = 1) in;
 void main()
 {    
     const int x = int(gl_GlobalInvocationID.x);
     const int y = int(gl_GlobalInvocationID.y);
     
-    
+    int PlaneIndex = int(gl_WorkGroupID.y);
+
+    int Count = imageLoad(cs_Histogram, ivec2(gl_GlobalInvocationID.xy)).x;
+
+    if (Count > 500)
+    {
+        float D = BinToPlaneDistance(gl_GlobalInvocationID.x, g_AzimuthBinCount);
+    }
 }
 
 #endif // __INCLUDE_CS_PLANE_EXTRACTION_GLSL__
