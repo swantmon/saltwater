965530526f607a280eaefe31db71f02d1f65067b
Unknown <kuno1234@gmx.de>
2018-02-08 12:50:15 +0100

    Find max value in histogram

--- data/graphic/shaders/scalable_kinect_fusion/plane_detection/cs_plane_extraction.glsl
+++ data/graphic/shaders/scalable_kinect_fusion/plane_detection/cs_plane_extraction.glsl
@@ -21,6 +21,8 @@ layout (binding = 2, MAP_TEXTURE_FORMAT) uniform image2D cs_NormalMap;
 // Functions
 // -------------------------------------------------------------------------------------
 
+shared int g_Counts[HISTOGRAM_WIDTH];
+
 layout (local_size_x = HISTOGRAM_WIDTH, local_size_y = 1, local_size_z = 1) in;
 void main()
 {    
@@ -30,23 +32,22 @@ void main()
 
     const int Count = imageLoad(cs_Histogram, ivec2(gl_GlobalInvocationID.xz)).x;
 
-    if (Count > 50)
+    g_Counts[gl_LocalInvocationIndex] = Count;
+
+    barrier();
+
+    for (int i = HISTOGRAM_WIDTH / 2; i > 0; i /= 2)
     {
-        bool IsHotSpot = true;
+        g_Counts[gl_LocalInvocationIndex] = max(g_Counts[gl_LocalInvocationIndex + i], g_Counts[gl_LocalInvocationIndex]);
 
-        for(int i = -2; i <= 2; ++ i)
-        {
-            if (imageLoad(cs_Histogram, ivec2(gl_GlobalInvocationID.xz)).x > Count)
-            {
-                IsHotSpot = false;
-            }
-        }
-
-        if (IsHotSpot)
-        {
-            float D = BinToPlaneDistance(int(gl_GlobalInvocationID.x), g_AzimuthBinCount);
-            g_Planes[PlaneIndex].w = -D;
-        }
+        barrier();
+    }
+    
+    if (g_Counts[0] == Count)
+    {
+        float D = BinToPlaneDistance(int(gl_GlobalInvocationID.x), g_AzimuthBinCount);
+        imageStore(cs_Histogram, ivec2(0, gl_GlobalInvocationID.z), ivec4(Count));
+        g_Planes[PlaneIndex].w = -D;
     }
 }
 
