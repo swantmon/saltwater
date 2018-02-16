37941cee545e29189ebcc98cc7e59fd4c2134a48
Tobias Schwandt <t.schwandt@gmail.com>
2018-02-16 13:21:23 +0100

    Add basic GUI component frame

---
+++ intern/src/editor_gui/edit_component.cpp
@@ -0,0 +1,20 @@
+﻿
+#include "edit_component.h"
+
+namespace Edit
+{
+    CComponent::CComponent(QWidget* _pParent)
+        : QWidget(_pParent)
+    {
+        // -----------------------------------------------------------------------------
+        // Setup UI
+        // -----------------------------------------------------------------------------
+        setupUi(this);
+    }
+
+    // -----------------------------------------------------------------------------
+
+    CComponent::~CComponent()
+    {
+    }
+} // namespace Edit
