348c6bf5349a71eddceb0442c547e42f714a38bc
Tobias Schwandt <t.schwandt@gmail.com>
2018-03-13 17:28:42 +0100

    Add transformation from one coord system to another

--- intern/test/base/test_base_coordinate_system.cpp
+++ intern/test/base/test_base_coordinate_system.cpp
@@ -7,11 +7,49 @@
 
 BASE_TEST(Test_Base_Coordinate_System)
 {
-    glm::mat3 In(1.0f);
+    auto EpsilonEqual = [&](const glm::mat3& _rLeft, const glm::mat3& _rRight, float _Epsilon) -> bool
+    {
+        return glm::all(glm::epsilonEqual(_rLeft[0], _rRight[0], _Epsilon))
+            && glm::all(glm::epsilonEqual(_rLeft[1], _rRight[1], _Epsilon))
+            && glm::all(glm::epsilonEqual(_rLeft[2], _rRight[2], _Epsilon));
+    };
 
-    glm::mat3 Base1 = Base::CCoordinateSystem::GetBaseMatrix<Base::CCoordinateSystem::Left, Base::CCoordinateSystem::Right>();
+    // -----------------------------------------------------------------------------
 
-    glm::mat3 Base2 = Base::CCoordinateSystem::GetBaseMatrix<Base::CCoordinateSystem::Right, Base::CCoordinateSystem::Left>();
+    glm::vec3 Forward;
+    glm::vec3 Right;
+    glm::vec3 Up;
+    glm::mat3 Base;
 
-    BASE_CHECK(Base1 == Base2);
+    // -----------------------------------------------------------------------------
+
+    Forward = glm::vec3(0.0f, 0.0f, -1.0f);
+    Right   = glm::vec3(1.0f, 0.0f,  0.0f);
+    Up      = glm::vec3(0.0f, 1.0f,  0.0f);
+
+    Base = Base::CCoordinateSystem::GetBaseMatrix(Right, Up, Forward);
+
+    auto T = glm::mat3(glm::eulerAngleX(glm::radians(-90.0f)));
+
+    BASE_CHECK(EpsilonEqual(Base, glm::mat3(glm::eulerAngleX(glm::radians(-90.0f))), 0.01f));
+
+    // -----------------------------------------------------------------------------
+
+    Forward = glm::vec3(0.0f,  0.0f, 1.0f);
+    Right   = glm::vec3(1.0f,  0.0f, 0.0f);
+    Up      = glm::vec3(0.0f, -1.0f, 0.0f);
+
+    Base = Base::CCoordinateSystem::GetBaseMatrix(Right, Up, Forward);
+
+    BASE_CHECK(EpsilonEqual(Base, glm::mat3(glm::eulerAngleX(glm::radians(90.0f))), 0.01f));
+
+    // -----------------------------------------------------------------------------
+
+    Forward = glm::vec3(0.0f, -1.0f,  0.0f);
+    Right   = glm::vec3(1.0f,  0.0f,  0.0f);
+    Up      = glm::vec3(0.0f,  0.0f, -1.0f);
+
+    Base = Base::CCoordinateSystem::GetBaseMatrix(Right, Up, Forward);
+
+    BASE_CHECK(EpsilonEqual(Base, glm::mat3(glm::eulerAngleX(glm::radians(180.0f))), 0.01f));
 }
