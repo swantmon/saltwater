715f50dce2709dda1c9ada9c95fa3207769cfb13
Tobias Schwandt <t.schwandt@gmail.com>
2018-04-12 13:11:13 +0200

    Remove unused UV coord in shader

--- data/plugins/light_estimation_lut/fs.glsl
+++ data/plugins/light_estimation_lut/fs.glsl
@@ -14,7 +14,6 @@ layout(binding = 1) uniform samplerCube in_LookUpTexture;
 // Input to fragment from VS
 // -----------------------------------------------------------------------------
 layout(location = 0) in vec3 in_Normal;
-layout(location = 1) in vec2 in_UV;
 
 // -----------------------------------------------------------------------------
 // Output to fragment
