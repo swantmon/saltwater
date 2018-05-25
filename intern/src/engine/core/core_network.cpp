ab8c3a7472dda82f52b315c9d62c405cd29852be
Unknown <kuno1234@gmx.de>
2018-05-25 14:01:32 +0200

    Add asio and zlib to project

--- intern/src/engine/core/core_network.cpp
+++ intern/src/engine/core/core_network.cpp
@@ -13,8 +13,6 @@ namespace Core
     }
 } // namespace Core
 
-using namespace nlohmann;
-
 namespace Core
 {
     CNetwork::CNetwork()
--- intern/src/engine/core/core_network.h
+++ intern/src/engine/core/core_network.h
@@ -3,13 +3,6 @@
 
 #include "engine/engine_config.h"
 
-#include "base/base_exception.h"
-#include "base/base_json.h"
-
-#include "engine/core/core_console.h"
-
-#include <string>
-
 namespace Core
 {
     class ENGINE_API CNetwork
