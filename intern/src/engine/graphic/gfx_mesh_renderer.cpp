d06f03dc38888cf29570a3537d48eb9f0a32c6ae
Tobias Schwandt <t.schwandt@gmail.com>
2018-06-13 15:36:40 +0200

    Add first implementation

--- intern/src/engine/graphic/gfx_mesh_renderer.cpp
+++ intern/src/engine/graphic/gfx_mesh_renderer.cpp
@@ -17,6 +17,7 @@
 
 #include "engine/graphic/gfx_buffer_manager.h"
 #include "engine/graphic/gfx_context_manager.h"
+#include "engine/graphic/gfx_histogram_renderer.h"
 #include "engine/graphic/gfx_main.h"
 #include "engine/graphic/gfx_material.h"
 #include "engine/graphic/gfx_material_manager.h"
@@ -24,6 +25,7 @@
 #include "engine/graphic/gfx_mesh_manager.h"
 #include "engine/graphic/gfx_mesh_renderer.h"
 #include "engine/graphic/gfx_performance.h"
+#include "engine/graphic/gfx_reflection_renderer.h"
 #include "engine/graphic/gfx_sampler_manager.h"
 #include "engine/graphic/gfx_shader_manager.h"
 #include "engine/graphic/gfx_state_manager.h"
@@ -73,6 +75,10 @@ namespace
 
     private:
 
+        static const unsigned int s_MaxNumberOfLights = 4;
+
+    private:
+
         struct SPerDrawCallConstantBufferVS
         {
             glm::mat4 m_ModelMatrix;
@@ -81,6 +87,19 @@ namespace
         struct SHitProxyProperties
         {
             unsigned int m_ID;
+        };
+
+        struct SLightProperties
+        {
+            glm::mat4    m_LightViewProjection;
+            glm::vec4    m_LightPosition;
+            glm::vec4    m_LightDirection;
+            glm::vec4    m_LightColor;
+            glm::vec4    m_LightSettings;
+            unsigned int m_LightType;
+            unsigned int m_Padding0;
+            unsigned int m_Padding1;
+            unsigned int m_Padding2;
         };
 
         struct SRenderJob
@@ -92,19 +111,30 @@ namespace
             glm::mat4        m_ModelMatrix;
         };
 
+        struct SLightJob
+        {
+            CTexturePtr m_ShadowTexturePtrs[s_MaxNumberOfLights];
+            CTexturePtr m_SpecularTexturePtr;
+            CTexturePtr m_DiffuseTexturePtr;
+        };
+
     private:
 
         typedef std::vector<SRenderJob> CRenderJobs;
+        typedef std::vector<SLightJob> CLightJobs;
 
     private:
 
         CBufferPtr        m_ModelBufferPtr;
         CBufferPtr        m_SurfaceMaterialBufferPtr;
         CBufferPtr        m_HitProxyPassPSBufferPtr;
+        CBufferPtr        m_LightPropertiesBufferPtr;
         CShaderPtr        m_HitProxyShaderPtr;
         CRenderContextPtr m_DeferredContextPtr;
         CRenderContextPtr m_HitProxyContextPtr;
         CRenderJobs       m_DeferredRenderJobs;
+        CRenderJobs       m_ForwardRenderJobs;
+        SLightJob         m_ForwardLightJobs;
 
     private:
 
@@ -118,15 +148,19 @@ namespace
         : m_ModelBufferPtr          ()
         , m_SurfaceMaterialBufferPtr()
         , m_HitProxyPassPSBufferPtr ()
+        , m_LightPropertiesBufferPtr()
         , m_HitProxyShaderPtr       ()
         , m_DeferredContextPtr      ()
         , m_HitProxyContextPtr      ()
         , m_DeferredRenderJobs      ()
+        , m_ForwardRenderJobs       ()
+        , m_ForwardLightJobs        ()
     {
         // -----------------------------------------------------------------------------
         // Reserve some jobs
         // -----------------------------------------------------------------------------
-        m_DeferredRenderJobs.reserve(1024);
+        m_DeferredRenderJobs.reserve(256);
+        m_ForwardRenderJobs.reserve(32);
     }
 
     // -----------------------------------------------------------------------------
@@ -150,6 +184,7 @@ namespace
         m_ModelBufferPtr           = 0;
         m_SurfaceMaterialBufferPtr = 0;
         m_HitProxyPassPSBufferPtr  = 0;
+        m_LightPropertiesBufferPtr = 0;
         m_HitProxyShaderPtr        = 0;
         m_DeferredContextPtr       = 0;
         m_HitProxyContextPtr       = 0;
@@ -157,16 +192,31 @@ namespace
         // -----------------------------------------------------------------------------
         // Iterate throw render jobs to release managed pointer
         // -----------------------------------------------------------------------------
-        CRenderJobs::const_iterator EndOfRenderJobs;
-
-        EndOfRenderJobs  = m_DeferredRenderJobs.end();
-
-        for (CRenderJobs::iterator CurrentRenderJob = m_DeferredRenderJobs.begin(); CurrentRenderJob != EndOfRenderJobs; ++ CurrentRenderJob)
+        for (auto CurrentRenderJob : m_DeferredRenderJobs)
         {
-            CurrentRenderJob->m_SurfacePtr = nullptr;
+            CurrentRenderJob.m_SurfacePtr = nullptr;
         }
 
         m_DeferredRenderJobs.clear();
+
+        // -----------------------------------------------------------------------------
+
+        for (auto CurrentRenderJob : m_ForwardRenderJobs)
+        {
+            CurrentRenderJob.m_SurfacePtr = nullptr;
+        }
+
+        m_ForwardRenderJobs.clear();
+
+        // -----------------------------------------------------------------------------
+
+        for (unsigned int IndexOfTexture = 0; IndexOfTexture < s_MaxNumberOfLights; ++IndexOfTexture)
+        {
+            m_ForwardLightJobs.m_ShadowTexturePtrs[IndexOfTexture] = nullptr;
+        }
+
+        m_ForwardLightJobs.m_DiffuseTexturePtr  = nullptr;
+        m_ForwardLightJobs.m_SpecularTexturePtr = nullptr;
     }
 
     // -----------------------------------------------------------------------------
@@ -267,6 +317,18 @@ namespace
         ConstanteBufferDesc.m_pClassKey     = 0;
 
         m_HitProxyPassPSBufferPtr = BufferManager::CreateBuffer(ConstanteBufferDesc);
+
+        // -----------------------------------------------------------------------------
+
+        ConstanteBufferDesc.m_Stride        = 0;
+        ConstanteBufferDesc.m_Usage         = CBuffer::GPURead;
+        ConstanteBufferDesc.m_Binding       = CBuffer::ConstantBuffer;
+        ConstanteBufferDesc.m_Access        = CBuffer::CPUWrite;
+        ConstanteBufferDesc.m_NumberOfBytes = sizeof(SLightProperties);
+        ConstanteBufferDesc.m_pBytes        = 0;
+        ConstanteBufferDesc.m_pClassKey     = 0;
+
+        m_LightPropertiesBufferPtr = BufferManager::CreateBuffer(ConstanteBufferDesc);
     }
 
     // -----------------------------------------------------------------------------
@@ -441,7 +503,147 @@ namespace
 
     void CGfxMeshRenderer::RenderForward()
     {
+        return;
 
+        Performance::BeginEvent("Actors");
+
+        ContextManager::SetTargetSet(TargetSetManager::GetLightAccumulationTargetSet());
+        ContextManager::SetViewPortSet(ViewManager::GetViewPortSet());
+        ContextManager::SetBlendState(StateManager::GetBlendState(CBlendState::AlphaBlend));
+        ContextManager::SetDepthStencilState(StateManager::GetDepthStencilState(0));
+        ContextManager::SetRasterizerState(StateManager::GetRasterizerState(0));
+
+        ContextManager::SetTopology(STopology::TriangleList);
+
+        ContextManager::SetConstantBuffer(0, m_ModelBufferPtr);
+        ContextManager::SetConstantBuffer(3, m_SurfaceMaterialBufferPtr);
+        ContextManager::SetConstantBuffer(4, Main::GetPerFrameConstantBuffer());
+
+        ContextManager::SetResourceBuffer(0, HistogramRenderer::GetExposureHistoryBuffer());
+        ContextManager::SetResourceBuffer(1, m_LightPropertiesBufferPtr);
+
+        // -----------------------------------------------------------------------------
+        // Bind shadow and reflection textures
+        // -----------------------------------------------------------------------------
+        ContextManager::SetSampler(6, SamplerManager::GetSampler(CSampler::MinMagMipLinearClamp));
+
+        ContextManager::SetTexture(6, ReflectionRenderer::GetBRDF());
+
+        if (m_ForwardLightJobs.m_SpecularTexturePtr != 0)
+        {
+            ContextManager::SetSampler(7, SamplerManager::GetSampler(CSampler::MinMagMipLinearClamp));
+
+            ContextManager::SetTexture(7, m_ForwardLightJobs.m_SpecularTexturePtr);
+        }
+
+        if (m_ForwardLightJobs.m_DiffuseTexturePtr != 0)
+        {
+            ContextManager::SetSampler(8, SamplerManager::GetSampler(CSampler::MinMagMipLinearClamp));
+
+            ContextManager::SetTexture(8, m_ForwardLightJobs.m_DiffuseTexturePtr);
+        }
+
+        for (unsigned int IndexOfTexture = 0; IndexOfTexture < s_MaxNumberOfLights; ++IndexOfTexture)
+        {
+            if (m_ForwardLightJobs.m_ShadowTexturePtrs[IndexOfTexture] != 0)
+            {
+                ContextManager::SetSampler(9 + IndexOfTexture, SamplerManager::GetSampler(CSampler::MinMagLinearMipPointClamp));
+
+                ContextManager::SetTexture(9 + IndexOfTexture, m_ForwardLightJobs.m_ShadowTexturePtrs[IndexOfTexture]);
+            }
+        }
+
+        // -----------------------------------------------------------------------------
+        // Actors
+        // -----------------------------------------------------------------------------
+        for (auto RenderJob : m_ForwardRenderJobs)
+        {
+            const CMaterial* pMaterial = RenderJob.m_SurfaceMaterialPtr;
+
+            // -----------------------------------------------------------------------------
+            // Upload data to buffer
+            // -----------------------------------------------------------------------------
+            SPerDrawCallConstantBufferVS ModelBuffer;
+
+            ModelBuffer.m_ModelMatrix = RenderJob.m_ModelMatrix;
+
+            BufferManager::UploadBufferData(m_ModelBufferPtr, &ModelBuffer);
+
+            BufferManager::UploadBufferData(m_SurfaceMaterialBufferPtr, &pMaterial->GetMaterialAttributes());
+
+            // -----------------------------------------------------------------------------
+            // Surface
+            // -----------------------------------------------------------------------------
+            CSurfacePtr SurfacePtr = RenderJob.m_SurfacePtr;
+
+            // -----------------------------------------------------------------------------
+            // Set shader
+            // -----------------------------------------------------------------------------
+            ContextManager::SetShaderVS(SurfacePtr->GetMVPShaderVS());
+
+            ContextManager::SetShaderPS(pMaterial->GetForwardShaderPS());
+
+            // -----------------------------------------------------------------------------
+            // Set textures
+            // -----------------------------------------------------------------------------
+            for (unsigned int IndexOfTexture = 0; IndexOfTexture < pMaterial->GetTextureSetPS()->GetNumberOfTextures(); ++IndexOfTexture)
+            {
+                ContextManager::SetSampler(IndexOfTexture, pMaterial->GetSamplerSetPS()->GetSampler(IndexOfTexture));
+
+                ContextManager::SetTexture(IndexOfTexture, pMaterial->GetTextureSetPS()->GetTexture(IndexOfTexture));
+            }
+
+            // -----------------------------------------------------------------------------
+            // Render
+            // -----------------------------------------------------------------------------
+            ContextManager::SetVertexBuffer(SurfacePtr->GetVertexBuffer());
+
+            ContextManager::SetIndexBuffer(SurfacePtr->GetIndexBuffer(), 0);
+
+            ContextManager::SetInputLayout(SurfacePtr->GetMVPShaderVS()->GetInputLayout());
+
+            ContextManager::DrawIndexed(SurfacePtr->GetNumberOfIndices(), 0, 0);
+        }
+
+        for (unsigned int IndexOfTexture = 0; IndexOfTexture < 16; ++IndexOfTexture)
+        {
+            ContextManager::ResetSampler(IndexOfTexture);
+
+            ContextManager::ResetTexture(IndexOfTexture);
+        }
+
+        ContextManager::ResetInputLayout();
+
+        ContextManager::ResetIndexBuffer();
+
+        ContextManager::ResetVertexBuffer();
+
+        ContextManager::ResetConstantBuffer(0);
+        ContextManager::ResetConstantBuffer(1);
+        ContextManager::ResetConstantBuffer(2);
+        ContextManager::ResetConstantBuffer(3);
+
+        ContextManager::ResetShaderVS();
+
+        ContextManager::ResetShaderDS();
+
+        ContextManager::ResetShaderHS();
+
+        ContextManager::ResetShaderGS();
+
+        ContextManager::ResetShaderPS();
+
+        ContextManager::ResetTopology();
+
+        ContextManager::ResetRasterizerState();
+
+        ContextManager::ResetDepthStencilState();
+
+        ContextManager::ResetViewPortSet();
+
+        ContextManager::ResetTargetSet();
+
+        Performance::EndEvent();
     }
 
     // -----------------------------------------------------------------------------
@@ -586,7 +788,14 @@ namespace
                 NewRenderJob.m_SurfaceMaterialPtr = pMaterial;
                 NewRenderJob.m_ModelMatrix        = rCurrentEntity.GetTransformationFacet()->GetWorldMatrix();
 
-                m_DeferredRenderJobs.push_back(NewRenderJob);
+                if (pMaterial->GetHasAlpha())
+                {
+                    m_ForwardRenderJobs.push_back(NewRenderJob);
+                }
+                else
+                {
+                    m_DeferredRenderJobs.push_back(NewRenderJob);
+                }
             }
         }
 
