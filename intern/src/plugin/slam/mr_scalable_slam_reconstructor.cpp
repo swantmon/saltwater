67614d5cbd04667333fb519606585305f634330e
Unknown <kuno1234@gmx.de>
2018-07-20 11:55:23 +0200

    Fix typo

--- intern/src/plugin/slam/mr_scalable_slam_reconstructor.cpp
+++ intern/src/plugin/slam/mr_scalable_slam_reconstructor.cpp
@@ -506,7 +506,7 @@ namespace MR
             << "#define VOXELS_PER_LEVEL2GRID "  << m_ReconstructionSettings.m_VoxelsPerGrid[2]     << " \n"
             << "#define RAYCAST_NEAR "           << m_DepthBounds.x                                 << " \n"
             << "#define RAYCAST_FAR "            << m_DepthBounds.y                                 << " \n"
-            << "#define VOLUME_DEPTH_THRESHLD "  << m_VolumeDepthThreshold                          << " \n"
+            << "#define VOLUME_DEPTH_THRESHOLD " << m_VolumeDepthThreshold                          << " \n"
             << "#define MIN_TREE_WEIGHT "        << m_MinWeight                                     << " \n";
         
         if (m_ReconstructionSettings.m_CaptureColor)
