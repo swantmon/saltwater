635380acd6cb84d3fc8aaa691d80d8b0715e1e4d
Unknown <kuno1234@gmx.de>
2018-09-18 15:43:56 +0200

    Move script to plugin

--- intern/src/plugin/slam/slam_plugin_interface.cpp
+++ intern/src/plugin/slam/slam_plugin_interface.cpp
@@ -14,76 +14,6 @@ CORE_PLUGIN_INFO(SLAM::CPluginInterface, "SLAM", "1.0", "This plugin provides Si
 
 namespace SLAM
 {
-    void CPluginInterface::InitializeReconstructor()
-    {
-        Gfx::ReconstructionRenderer::GetReconstructor().Start();
-    }
-
-    // -----------------------------------------------------------------------------
-
-    void CPluginInterface::TerminateReconstructor()
-    {
-        Gfx::ReconstructionRenderer::GetReconstructor().Exit();
-    }
-
-    // -----------------------------------------------------------------------------
-
-    void CPluginInterface::OnNewFrame(Gfx::CTexturePtr DepthBuffer, Gfx::CTexturePtr ColorBuffer, const glm::mat4* pTransform)
-    {
-        Gfx::ReconstructionRenderer::GetReconstructor().OnNewFrame(DepthBuffer, ColorBuffer, pTransform);
-    }
-
-    // -----------------------------------------------------------------------------
-
-    void CPluginInterface::SetImageSizesAndIntrinsicData(glm::vec4 _ImageSizes, glm::vec4 _Intrinsics)
-    {
-        glm::ivec2 DepthSize = glm::ivec2(_ImageSizes.x, _ImageSizes.y);
-        glm::ivec2 ColorSize = glm::ivec2(glm::max(_ImageSizes.z, 1.0f), glm::max(_ImageSizes.w, 1.0f));
-
-        glm::vec2 FocalLength = glm::vec2(_Intrinsics.x, _Intrinsics.y);
-        glm::vec2 FocalPoint = glm::vec2(_Intrinsics.z, _Intrinsics.w);
-        
-        Gfx::ReconstructionRenderer::GetReconstructor().SetImageSizes(DepthSize, ColorSize);
-        Gfx::ReconstructionRenderer::GetReconstructor().SetIntrinsics(FocalLength, FocalPoint);
-    }
-
-    // -----------------------------------------------------------------------------
-
-    void CPluginInterface::ResetReconstruction()
-    {
-        Gfx::ReconstructionRenderer::GetReconstructor().ResetReconstruction();
-    }
-
-    // -----------------------------------------------------------------------------
-
-    glm::mat4 CPluginInterface::GetPoseMatrix()
-    {
-        return Gfx::ReconstructionRenderer::GetReconstructor().GetPoseMatrix();
-    }
-
-    // -----------------------------------------------------------------------------
-
-    glm::vec3 CPluginInterface::Pick(const glm::ivec2 _rCursor)
-    {
-        return Gfx::ReconstructionRenderer::Pick(_rCursor);
-    }
-
-    // -----------------------------------------------------------------------------
-
-    void CPluginInterface::SetSelectionBox(const glm::vec3& _rAnchor0, const glm::vec3& _rAnchor1, float _Height, int _State)
-    {
-        Gfx::ReconstructionRenderer::SetSelectionBox(_rAnchor0, _rAnchor1, _Height, _State);
-    }
-
-    // -----------------------------------------------------------------------------
-
-    bool CPluginInterface::IsCapturingColor()
-    {
-        MR::SReconstructionSettings Settings;
-        Gfx::ReconstructionRenderer::GetReconstructor().GetReconstructionSettings(&Settings);
-        return Settings.m_CaptureColor;
-    }
-
     // -----------------------------------------------------------------------------
 
     void CPluginInterface::OnStart()
@@ -92,19 +22,22 @@ namespace SLAM
 
         Engine::RegisterEventHandler(Engine::Gfx_OnUpdate, Gfx::ReconstructionRenderer::Update);
         Engine::RegisterEventHandler(Engine::Gfx_OnRenderGBuffer, Gfx::ReconstructionRenderer::Render);
+
+        m_SLAMControl.Start();
     }
 
     // -----------------------------------------------------------------------------
 
     void CPluginInterface::OnExit()
     {
-        Gfx::ReconstructionRenderer::OnExit();
+        m_SLAMControl.Exit();
     }
 
     // -----------------------------------------------------------------------------
 
     void CPluginInterface::Update()
     {
+        m_SLAMControl.Update();
     }
 
     // -----------------------------------------------------------------------------
@@ -122,47 +55,27 @@ namespace SLAM
     }
 } // namespace HW
 
-extern "C" CORE_PLUGIN_API_EXPORT void InitializeReconstructor()
+extern "C" CORE_PLUGIN_API_EXPORT void Start()
 {
-    static_cast<SLAM::CPluginInterface&>(GetInstance()).InitializeReconstructor();
+    static_cast<SLAM::CPluginInterface&>(GetInstance()).OnStart();
 }
 
-extern "C" CORE_PLUGIN_API_EXPORT void TerminateReconstructor()
+extern "C" CORE_PLUGIN_API_EXPORT void Exit()
 {
-    static_cast<SLAM::CPluginInterface&>(GetInstance()).TerminateReconstructor();
+    static_cast<SLAM::CPluginInterface&>(GetInstance()).OnExit();
 }
 
-extern "C" CORE_PLUGIN_API_EXPORT void OnNewDepthFrame(Gfx::CTexturePtr DepthFrame, Gfx::CTexturePtr ColorBuffer, const glm::mat4* pTransform)
+extern "C" CORE_PLUGIN_API_EXPORT void Pause()
 {
-    static_cast<SLAM::CPluginInterface&>(GetInstance()).OnNewFrame(DepthFrame, ColorBuffer, pTransform);
+    static_cast<SLAM::CPluginInterface&>(GetInstance()).OnPause();
 }
 
-extern "C" CORE_PLUGIN_API_EXPORT void SetImageSizesAndIntrinsicData(glm::vec4 _ImageSizes, glm::vec4 _Intrinsics)
+extern "C" CORE_PLUGIN_API_EXPORT void Resume()
 {
-    static_cast<SLAM::CPluginInterface&>(GetInstance()).SetImageSizesAndIntrinsicData(_ImageSizes, _Intrinsics);
+    static_cast<SLAM::CPluginInterface&>(GetInstance()).OnResume();
 }
 
-extern "C" CORE_PLUGIN_API_EXPORT void ResetReconstruction()
+extern "C" CORE_PLUGIN_API_EXPORT void Update()
 {
-    static_cast<SLAM::CPluginInterface&>(GetInstance()).ResetReconstruction();
-}
-
-extern "C" CORE_PLUGIN_API_EXPORT void GetPoseMatrix(glm::mat4& _PoseMatrix)
-{
-    _PoseMatrix = static_cast<SLAM::CPluginInterface&>(GetInstance()).GetPoseMatrix();
-}
-
-extern "C" CORE_PLUGIN_API_EXPORT void Pick(const glm::ivec2& _rCursor, glm::vec3 _rHit)
-{
-    _rHit = static_cast<SLAM::CPluginInterface&>(GetInstance()).Pick(_rCursor);
-}
-
-extern "C" CORE_PLUGIN_API_EXPORT void UpdateSelectionBox(const glm::vec3& _rAnchor0, const glm::vec3& _rAnchor1, float _Height, int _State)
-{
-    static_cast<SLAM::CPluginInterface&>(GetInstance()).SetSelectionBox(_rAnchor0, _rAnchor1, _Height, _State);
-}
-
-extern "C" CORE_PLUGIN_API_EXPORT bool IsCapturingColor(void)
-{
-    return static_cast<SLAM::CPluginInterface&>(GetInstance()).IsCapturingColor();
+    static_cast<SLAM::CPluginInterface&>(GetInstance()).Update();
 }
--- intern/src/plugin/slam/slam_plugin_interface.h
+++ intern/src/plugin/slam/slam_plugin_interface.h
@@ -5,22 +5,12 @@
 
 #include "plugin/slam/mr_scalable_slam_reconstructor.h"
 
+#include "plugin/slam/mr_slam_control.h"
+
 namespace SLAM
 {
     class CPluginInterface : public Core::IPlugin
     {
-    public:
-
-        void InitializeReconstructor();
-        void TerminateReconstructor();
-        void OnNewFrame(Gfx::CTexturePtr, Gfx::CTexturePtr ColorBuffer, const glm::mat4* pTransform);
-        void SetImageSizesAndIntrinsicData(glm::vec4 _ImageSizes, glm::vec4 _Intrinsics);
-        void ResetReconstruction();
-        glm::mat4 GetPoseMatrix();
-        glm::vec3 Pick(const glm::ivec2 _rCursor);
-        void SetSelectionBox(const glm::vec3& _rAnchor0, const glm::vec3& _rAnchor1, float _Height, int _State);
-        bool IsCapturingColor();
-
     public:
 
         void OnStart() override;
@@ -31,6 +21,6 @@ namespace SLAM
 
     private:
 
-
+        MR::CSLAMControl m_SLAMControl;
     };
 } // namespace HW
