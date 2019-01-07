ed477b03b55c1d2128a07c65462eae6aef796ca4
Tobias Schwandt <t.schwandt@gmail.com>
2019-01-07 15:57:49 +0100

    Add some functionality to test case

--- intern/test/core/test_core_recorder.cpp
+++ intern/test/core/test_core_recorder.cpp
@@ -7,5 +7,40 @@
 
 BASE_TEST(RecordDataWithRecorder)
 {
+    // -----------------------------------------------------------------------------
+    // Data
+    // -----------------------------------------------------------------------------
+    int IntegerValue = 4;
+    float FloatingValue = 13.37f;
+    double DoubleValue = 12.34;
+    char* pCharValue = "This is a test";
+    std::string StringValue = "This is just another test!";
+
+    struct SComplex
+    {
+        int m_Header;
+        float m_Value;
+    };
+
+    SComplex ComplexStructValue = { 0, 1.0 };
+
+
+    // -----------------------------------------------------------------------------
+    // Recording
+    // -----------------------------------------------------------------------------
     Core::CRecorder NewRecorder;
+
+    NewRecorder.Dump(&IntegerValue, sizeof(IntegerValue));
+    NewRecorder.Dump(&FloatingValue, sizeof(FloatingValue));
+    NewRecorder.Dump(&pCharValue, strlen(pCharValue));
+
+    NewRecorder.Step();
+
+    NewRecorder.Dump(&DoubleValue, sizeof(DoubleValue));
+    NewRecorder.Dump(&StringValue, sizeof(StringValue));
+    NewRecorder.Dump(&ComplexStructValue, sizeof(SComplex));
+
+    NewRecorder.Step();
+
+    NewRecorder.SaveRecordingToFile("test_recording.7z");
 }
