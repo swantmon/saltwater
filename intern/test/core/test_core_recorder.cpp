08e0fe91088fc6fa23753b864a24c64b4584ce32
Tobias Schwandt <t.schwandt@gmail.com>
2019-01-13 15:01:16 +0100

    Add test for using recorder with file

--- intern/test/core/test_core_recorder.cpp
+++ intern/test/core/test_core_recorder.cpp
@@ -9,8 +9,10 @@
 #include "engine/core/core_record_writer.h"
 
 #include <array>
+#include <fstream>
+#include <sstream>
 
-BASE_TEST(RecordDataWithRecorder)
+BASE_TEST(RecordDataWithRecorderSStream)
 {
     // -----------------------------------------------------------------------------
     // Data
@@ -26,12 +28,12 @@ BASE_TEST(RecordDataWithRecorder)
 
     int Index = 0;
 
-    for (auto& Frame : Frames)
+    for (auto& OriginalFrame : Frames)
     {
-        Frame.m_1 = Index;
-        Frame.m_2 = 1337.0f - float(Index);
+        OriginalFrame.m_1 = Index;
+        OriginalFrame.m_2 = 1337.0f - float(Index);
 
-        Frame.m_3.resize(glm::linearRand(1, 4000000));
+        OriginalFrame.m_3.resize(glm::linearRand(1, 4000000));
 
         ++Index;
     }
@@ -43,12 +45,12 @@ BASE_TEST(RecordDataWithRecorder)
 
     Core::CRecordWriter RecordWriter(Stream, 1);
 
-    for (auto& Frame : Frames)
+    for (auto& OriginalFrame : Frames)
     {
-        RecordWriter << Frame.m_1;
-        RecordWriter << Frame.m_2;
+        RecordWriter << OriginalFrame.m_1;
+        RecordWriter << OriginalFrame.m_2;
 
-        Base::Write(RecordWriter, Frame.m_3);
+        Base::Write(RecordWriter, OriginalFrame.m_3);
     }
 
     // -----------------------------------------------------------------------------
@@ -58,20 +60,114 @@ BASE_TEST(RecordDataWithRecorder)
 
     // -----------------------------------------------------------------------------
     // Check record w/ prev. record
-    // -----------------------------------------------------------------------------    
-    for (auto& Frame : Frames)
+    // -----------------------------------------------------------------------------
+    for (auto& OriginalFrame : Frames)
     {
         RecordReader.Update();
 
-        SFrame FrameCheck;
+        SFrame ReadFrame;
 
-        RecordReader >> FrameCheck.m_1;
-        RecordReader >> FrameCheck.m_2;
+        RecordReader >> ReadFrame.m_1;
+        RecordReader >> ReadFrame.m_2;
 
-        Base::Read(RecordReader, FrameCheck.m_3);
+        Base::Read(RecordReader, ReadFrame.m_3);
 
-        BASE_CHECK(Frame.m_1 == FrameCheck.m_1);
-        BASE_CHECK(Frame.m_2 == FrameCheck.m_2);
-        BASE_CHECK(Frame.m_3.size() == FrameCheck.m_3.size());
+        BASE_CHECK(OriginalFrame.m_1 == ReadFrame.m_1);
+        BASE_CHECK(OriginalFrame.m_2 == ReadFrame.m_2);
+        BASE_CHECK(OriginalFrame.m_3.size() == ReadFrame.m_3.size());
     }
+}
+
+BASE_TEST(RecordDataWithRecorderFStream)
+{
+    // -----------------------------------------------------------------------------
+    // Data
+    // -----------------------------------------------------------------------------
+    struct SFrame
+    {
+        int   m_1;
+        float m_2;
+        std::vector<char> m_3;
+    };
+
+    std::array<SFrame, 120> Frames;
+
+    int Index = 0;
+
+    for (auto& OriginalFrame : Frames)
+    {
+        OriginalFrame.m_1 = Index;
+        OriginalFrame.m_2 = 1337.0f - float(Index);
+
+        OriginalFrame.m_3.resize(glm::linearRand(1, 4000));
+
+        for (auto& rChar : OriginalFrame.m_3)
+        {
+            //rChar = glm::linearRand(0, 255);
+        }
+
+        ++Index;
+    }
+
+    // -----------------------------------------------------------------------------
+    // Recording
+    // -----------------------------------------------------------------------------
+    std::ofstream OutFileStream;
+
+    OutFileStream.open("test.rec", std::ofstream::binary);
+
+    BASE_CHECK(OutFileStream.is_open());
+
+    Core::CRecordWriter RecordWriter(OutFileStream, 1);
+
+    for (auto& OriginalFrame : Frames)
+    {
+        RecordWriter << OriginalFrame.m_1;
+        RecordWriter << OriginalFrame.m_2;
+
+        Base::Write(RecordWriter, OriginalFrame.m_3);
+    }
+
+    OutFileStream.close();
+
+    // -----------------------------------------------------------------------------
+    // Read record
+    // -----------------------------------------------------------------------------
+    std::ifstream InFileStream;
+
+    InFileStream.open("test.rec", std::ifstream::binary);
+
+    BASE_CHECK(InFileStream.is_open());
+
+    Core::CRecordReader RecordReader(InFileStream, 1);
+
+    // -----------------------------------------------------------------------------
+    // Check record w/ prev. record
+    // -----------------------------------------------------------------------------
+    for (auto& OriginalFrame : Frames)
+    {
+        RecordReader.Update();
+
+        SFrame ReadFrame;
+
+        RecordReader >> ReadFrame.m_1;
+        RecordReader >> ReadFrame.m_2;
+
+        Base::Read(RecordReader, ReadFrame.m_3);
+
+        BASE_CHECK(OriginalFrame.m_1 == ReadFrame.m_1);
+        BASE_CHECK(OriginalFrame.m_2 == ReadFrame.m_2);
+        BASE_CHECK(OriginalFrame.m_3.size() == ReadFrame.m_3.size());
+
+        int Index = 0;
+
+        for (auto& rChar : OriginalFrame.m_3)
+        {
+            BASE_CHECK(rChar == ReadFrame.m_3[Index]);
+
+            ++Index;
+        }
+    }
+
+    InFileStream.close();
 }
