27c068a411b5bb80ab637661a1c4f3ba4ad08786
Unknown <kuno1234@gmx.de>
2019-01-18 15:32:36 +0100

    Show texture on plane

--- data/graphic/shaders/slam/scalable_kinect_fusion/rendering/fs_inpainted_plane.glsl
+++ data/graphic/shaders/slam/scalable_kinect_fusion/rendering/fs_inpainted_plane.glsl
@@ -10,8 +10,8 @@ layout(location = 0) out vec4 out_Color;
 
 void main()
 {
-    //out_Color = texture(PlaneTexture, in_TexCoords);
-    out_Color = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+    out_Color = texture(PlaneTexture, in_TexCoords);
+    //out_Color = vec4(1.0f, 0.0f, 0.0f, 1.0f);
 }
 
 #endif // __INCLUDE_FS_OUTLINE_GLSL__
