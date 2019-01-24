d92cbfe1b2c749647d354339a80456de89e881d4
Tobias Schwandt <t.schwandt@gmail.com>
2019-01-24 10:48:49 +0100

    Add more values to the material

--- intern/src/editor_imgui/edit_data_material_component.h
+++ intern/src/editor_imgui/edit_data_material_component.h
@@ -14,7 +14,20 @@ namespace Dt
         void OnGUI()
         {
             ImGui::ColorEdit4("Albedo", &m_Color.x);
+
             ImGui::SliderFloat("Roughness", &m_Roughness, 0.0f, 1.0f);
+
+            ImGui::SliderFloat("Reflectance", &m_Reflectance, 0.0f, 1.0f);
+
+            ImGui::SliderFloat("Metalness", &m_MetalMask, 0.0f, 1.0f);
+
+            ImGui::SliderFloat("Displacement", &m_Displacement, 0.0f, 1.0f);
+
+            ImGui::SliderFloat("Refraction", &m_RefractionIndex, 0.0f, 1.0f);
+
+            ImGui::DragFloat2("Tilling", &m_TilingOffset.x);
+
+            ImGui::DragFloat2("Offset", &m_TilingOffset.z);
         }
 
         // -----------------------------------------------------------------------------
