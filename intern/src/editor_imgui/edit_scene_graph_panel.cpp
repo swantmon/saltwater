ab9b1ed89a22359a28a6410c71034dbc8b40a318
Tobias Schwandt <t.schwandt@gmail.com>
2019-01-24 13:37:06 +0100

    Add default scene graph

--- intern/src/editor_imgui/edit_scene_graph_panel.cpp
+++ intern/src/editor_imgui/edit_scene_graph_panel.cpp
@@ -4,6 +4,7 @@
 #include "editor_imgui/edit_gui_factory.h"
 #include "editor_imgui/edit_scene_graph_panel.h"
 
+#include "engine/data/data_map.h"
 #include "engine/data/data_entity.h"
 #include "engine/data/data_entity_manager.h"
 #include "engine/data/data_hierarchy_facet.h"
@@ -30,11 +31,60 @@ namespace GUI
 
     void CSceneGraphPanel::Render()
     {
+        std::function<void(Dt::CEntity*)> RecursiveTree = [&](Dt::CEntity* _pEntity)->void
+        {
+            Dt::CEntity* pSibling = _pEntity;
+
+            while (pSibling != nullptr)
+            {
+                auto pHierarchyFacet = pSibling->GetHierarchyFacet();
+
+                if (pHierarchyFacet->GetFirstChild() == 0)
+                {
+                    ImGui::Indent();
+
+                    ImGui::Selectable(pSibling->GetName().c_str());
+
+                    ImGui::Unindent();
+                }
+                else
+                {
+                    if (ImGui::TreeNode("Test", "%s", pSibling->GetName().c_str()))
+                    {
+                        if (pHierarchyFacet->GetFirstChild() != 0)
+                        {
+                            RecursiveTree(pHierarchyFacet->GetFirstChild());
+                        }
+
+                        ImGui::TreePop();
+                    }
+                }
+
+                pSibling = pHierarchyFacet->GetSibling();
+            }
+        };
+
+
         Edit::CGUIFactory& rFactory = Edit::CGUIFactory::GetInstance();
 
         ImGui::Begin("Scene Graph");
 
-        
+        auto CurrentEntity = Dt::Map::EntitiesBegin();
+        auto EndOfEntities = Dt::Map::EntitiesEnd();
+
+        while (CurrentEntity != EndOfEntities)
+        {
+            auto pHierarchyFacet = CurrentEntity->GetHierarchyFacet();
+
+            Dt::CEntity* pEntity = &*CurrentEntity;
+
+            if (pHierarchyFacet->GetParent() == 0)
+            {
+                RecursiveTree(pEntity);
+            }
+
+            CurrentEntity = CurrentEntity.Next();
+        }
 
         ImGui::End();
     }
