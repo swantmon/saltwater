63c5852a58e6905e71930a20c8690ac98bf63a2e
Unknown <kuno1234@gmx.de>
2019-01-25 14:08:48 +0100

    Move shaders and change includes accordingly

---
+++ data/plugins/slam/scalable/plane_detection/cs_normal_histogram.glsl
@@ -0,0 +1,70 @@
+
+#ifndef __INCLUDE_CS_NORMAL_HISTOGRAM_GLSL__
+#define __INCLUDE_CS_NORMAL_HISTOGRAM_GLSL__
+
+#include "../../plugins/slam/scalable/plane_detection/common_plane_detection.glsl"
+
+// -----------------------------------------------------------------------------
+// Input from engine
+// -----------------------------------------------------------------------------
+
+layout (binding = 0, r32i) uniform iimage2D cs_Histogram;
+layout (binding = 1, MAP_TEXTURE_FORMAT) uniform image2D cs_VertexMap;
+layout (binding = 2, MAP_TEXTURE_FORMAT) uniform image2D cs_NormalMap;
+
+// -------------------------------------------------------------------------------------
+// Functions
+// -------------------------------------------------------------------------------------
+
+layout (local_size_x = TILE_SIZE2D, local_size_y = TILE_SIZE2D, local_size_z = 1) in;
+void main()
+{    
+    const int x = int(gl_GlobalInvocationID.x);
+    const int y = int(gl_GlobalInvocationID.y);
+
+    ivec2 ImageSize = imageSize(cs_VertexMap);
+
+    if (x >= ImageSize.x || y >= ImageSize.y)
+    {
+        return;
+    }
+
+    vec3 Normal = vec3(0.0f);
+    int Count = 0;
+    
+    for (int i = -g_KernelSize; i <= g_KernelSize; ++ i)
+    {
+        for (int j = -g_KernelSize; j <= g_KernelSize; ++ j)
+        {
+            if (x + i < ImageSize.x && x + i >= 0 &&
+                y + j < ImageSize.y && y + j >= 0)
+            {
+                vec3 Sample = mat3(g_InvPoseMatrix) * imageLoad(cs_NormalMap, ivec2(x + i, y + j)).xyz;
+
+                if (Sample.x > -5.0f)
+                {
+                    ++ Count;
+                    Normal += Sample;
+                }
+            }
+        }
+    }
+    
+    if (Count > 0)
+    {
+        Normal /= Count;
+    
+        //Normal = mat3(g_PoseMatrix) * Normal;
+
+        vec2 Spherical = CartesianToSpherical(Normal);
+
+        float Azimuth = Spherical.x;
+        float Inclination = Spherical.y;
+        
+        ivec2 Bin = SphericalToBin(Azimuth, Inclination, g_AzimuthBinCount, g_InclinationBinCount);
+
+        imageAtomicAdd(cs_Histogram, Bin, 1);
+    }
+}
+
+#endif // __INCLUDE_CS_NORMAL_HISTOGRAM_GLSL__
---
+++ data/plugins/slam/scalable/rasterization/cs_gather_full.glsl
@@ -0,0 +1,68 @@
+
+#ifndef __INCLUDE_CS_GATHER_GATHER_FULL_GLSL__
+#define __INCLUDE_CS_GATHER_GATHER_FULL_GLSL__
+
+#include "../../plugins/slam/scalable/common_scalable.glsl"
+#include "../../plugins/slam/scalable/common_indirect.glsl"
+
+// -----------------------------------------------------------------------------
+// Buffers
+// -----------------------------------------------------------------------------
+
+layout(binding = 1, r8ui) uniform uimage3D cs_Volume;
+
+layout(std430, binding = 0) buffer Level1IndirectBuffer
+{
+    SIndirectBuffers g_Level1Indirect;
+};
+
+layout(std430, binding = 1) buffer Level2IndirectBuffer
+{
+    SIndirectBuffers g_Level2Indirect;
+};
+
+layout(std430, binding = 2) buffer Level1Queue
+{
+    uint g_Level1VolumeID[];
+};
+
+layout(std430, binding = 3) buffer Level2Queue
+{
+    uint g_Level2VolumeID[];
+};
+
+// -----------------------------------------------------------------------------
+// Compute Shader
+// -----------------------------------------------------------------------------
+
+const int g_Resolution = ROOT_RESOLUTION * LEVEL1_RESOLUTION;
+
+shared int TaggedSum;
+
+layout (local_size_x = LEVEL1_RESOLUTION, local_size_y = LEVEL1_RESOLUTION, local_size_z = LEVEL1_RESOLUTION) in;
+void main()
+{
+    uint IsTagged = imageLoad(cs_Volume, ivec3(gl_GlobalInvocationID)).x;
+    imageStore(cs_Volume, ivec3(gl_GlobalInvocationID), uvec4(0));
+
+    if (gl_LocalInvocationIndex == 0)
+    {
+        TaggedSum = 0;
+    }
+
+    barrier();
+
+    if (IsTagged > 0)
+    {
+        uint Index = atomicAdd(g_Level2Indirect.m_Indexed.m_InstanceCount, 1);
+        g_Level2VolumeID[Index] = OffsetToIndex(vec3(gl_GlobalInvocationID), ROOT_RESOLUTION * LEVEL1_RESOLUTION);
+        
+        if (atomicAdd(TaggedSum, 1) == 1)
+        {
+            uint Index = atomicAdd(g_Level1Indirect.m_Indexed.m_InstanceCount, 1);
+            g_Level1VolumeID[Index] = OffsetToIndex(vec3(gl_WorkGroupID), ROOT_RESOLUTION);
+        }
+    }
+}
+
+#endif // __INCLUDE_CS_GATHER_GATHER_FULL_GLSL__
