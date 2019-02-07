f467a92324f31c02bab29aea034cee972a98454b
Kunert <kuno1234@gmx.de>
2019-02-07 15:12:04 +0100

    Make container private

--- intern/src/base/base_delegate.h
+++ intern/src/base/base_delegate.h
@@ -13,6 +13,7 @@ namespace PAT
     class CDelegate
     {
     public:
+
         using FunctionType = std::function<void(Args...)>;
         using HandleType = std::shared_ptr<FunctionType>;
         using ContainerType = std::vector<std::weak_ptr<FunctionType>>;
@@ -34,13 +35,15 @@ namespace PAT
             s_Container.erase(std::remove_if(s_Container.begin(), s_Container.end(), [](const auto& _Handle) { return _Handle.expired(); }), s_Container.end());
         }
 
-        static HandleType Register(FunctionType _Function)
+        static auto Register(FunctionType _Function)
         {
             auto Ptr = std::make_shared<FunctionType>(_Function);
             s_Container.push_back(Ptr);
             return Ptr;
         }
 
+    private:
+        
         static ContainerType s_Container;
     };
 
