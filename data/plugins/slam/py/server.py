fbfa78563f466ef395fe2f7264347652b14538e5
Kunert <kuno1234@gmx.de>
2019-02-20 10:35:42 +0100

    Combine images

--- data/plugins/slam/py/server.py
+++ data/plugins/slam/py/server.py
@@ -115,8 +115,7 @@ def OnNewClient(_Socket, _Address, _ID):
 
         for y in range(opt.img_size_h):
             for x in range(opt.img_size_w):
-                if panorama[y][x][3] != 0.0:
-                    masked_sample_pixels[x, y] = (int(panorama[y][x][0] * 255), int(panorama[y][x][1] * 255), int(panorama[y][x][2] * 255))
+                masked_sample_pixels[x, y] = (int(panorama[y][x][0] * 255), int(panorama[y][x][1] * 255), int(panorama[y][x][2] * 255))
 
         transform = transforms.Compose(transforms_)
 
@@ -153,17 +152,36 @@ def OnNewClient(_Socket, _Address, _ID):
         # -----------------------------------------------------------------------------
         # Save output and input to file system
         # -----------------------------------------------------------------------------
-        #sample = torch.cat((masked_samples.data, gen_mask.data), -2)
+        os.makedirs('{}{}/{}'.format(opt.output, _Address[0], _ID), exist_ok=True)
 
-        #os.makedirs('{}{}/{}'.format(opt.output, _Address[0], _ID), exist_ok=True)
-        #save_image(sample, '{}{}/{}/result_panorama_{}.png'.format(opt.output, _Address[0], _ID, Interval), nrow=1, normalize=True)
+        save_image(masked_sample.data, '{}{}/{}/masked_{}.png'.format(opt.output, _Address[0], _ID, Interval), nrow=1, normalize=True)
+        save_image(gen_mask.data, '{}{}/{}/generated_{}.png'.format(opt.output, _Address[0], _ID, Interval), nrow=1, normalize=True)
+
+        masked_image = np.asarray(list(Image.open('{}{}/{}/masked_{}.png'.format(opt.output, _Address[0], _ID, Interval)).convert('RGBA').getdata()))
+        gen_image = np.asarray(list(Image.open('{}{}/{}/generated_{}.png'.format(opt.output, _Address[0], _ID, Interval)).convert('RGBA').getdata()))
+
+        masked_image.shape = (128, 128, 4)
+        gen_image.shape = (64, 64, 4)
+
+        masked_image = masked_image.astype(np.uint8)
+        gen_image = gen_image.astype(np.uint8)
+
+        filled = masked_image.copy()
+
+        for _x in range(32, 96):
+            for _y in range(32, 96):
+                filled[_x][_y][0] = gen_image[_x - 32][_y - 32][0]
+                filled[_x][_y][1] = gen_image[_x - 32][_y - 32][1]
+                filled[_x][_y][2] = gen_image[_x - 32][_y - 32][2]
+                filled[_x][_y][3] = gen_image[_x - 32][_y - 32][3]
+
+        im = Image.fromarray(filled)
+        im.save('{}{}/{}/filled_{}.png'.format(opt.output, _Address[0], _ID, Interval))
+        #save_image(filled.data, '{}{}/{}/generated_{}.png'.format(opt.output, _Address[0], _ID, Interval), nrow=1, normalize=True)
 
         Interval = Interval + 1
 
     print ("Disconnected from client", _Address)
-
-    if os.path.isfile('{}{}/{}/tmp_output_generator.png'.format(opt.temp, _Address[0], _ID)) == True:
-        os.remove('{}}{}/{}/tmp_output_generator.png'.format(opt.temp, _Address[0], _ID))
     
     _Socket.close()
 
