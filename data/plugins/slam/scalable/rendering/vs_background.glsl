5b2c6fba5d8ec5e8a448f281a7ff3996526cd000
Kunert <kuno1234@gmx.de>
2019-03-06 14:31:17 +0100

    Add shaders for background rendering

---
+++ data/plugins/slam/scalable/rendering/vs_background.glsl
@@ -0,0 +1,25 @@
+
+#ifndef __INCLUDE_VS_OUTLINE_GLSL__
+#define __INCLUDE_VS_OUTLINE_GLSL__
+
+layout(location = 0) in vec2 in_VertexPosition;
+
+layout(location = 0) out vec2 out_TexCoord;
+
+out gl_PerVertex
+{
+    vec4 gl_Position;
+};
+
+// -----------------------------------------------------------------------------
+// Functions
+// -----------------------------------------------------------------------------
+
+void main()
+{
+    out_TexCoord = in_VertexPosition * 0.5f + 0.5f;
+    out_TexCoord.y = 1.0f - out_TexCoord.y;
+    gl_Position = vec4(in_VertexPosition, 0.0f, 1.0f);
+}
+
+#endif // __INCLUDE_VS_OUTLINE_GLSL__
