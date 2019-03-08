420e743f3269b6ce7daff4e08f56dbfcfb541ee8
ChaoYang.Fu_TU Ilmenau <chao-yang.fu@tu-ilmenau.de>
2019-03-08 09:32:56 +0100

    Build Function: determRhoRange

--- intern/src/plugin/stereo/stereo_PolarRect.h
+++ intern/src/plugin/stereo/stereo_PolarRect.h
@@ -17,18 +17,20 @@ namespace Stereo
     public:
         PolarRect();
         ~PolarRect();
+    
     //---Main Function---
     private:
         void determ_CoRegion(const std::vector<cv::Point2f>& epipoles, const cv::Size imgDimensions, const cv::Mat & F);
 
+
     //---Assist Function---
     private:
-        void getExternalPoints(const cv::Point2d& epipole, const cv::Size imgDimensions, std::vector<cv::Point2f>& externalPoints);
-        void determ_RhoRange(const cv::Point2d& epipole, const cv::Size imgDimensions, const std::vector<cv::Point2f>& externalPoints, double& minRho, double& maxRho);
+        void getExternalPoints(const cv::Point2d& EpiPole, const cv::Size ImgSize, std::vector<cv::Point2f>& ImgPt_Extern);
+        void determ_RhoRange(const cv::Point2d& EpiPole, const cv::Size ImgSize, const std::vector<cv::Point2f>& ImgPt_Extern, double& minRho, double& maxRho);
 
     //---Member---
     private:
-        double m_minRho1, m_maxRho1, m_minRho2, m_maxRho2;
+        double m_minRho1, m_maxRho1, m_minRho2, m_maxRho2; // Min & Max distance from Epipole to Image Plane.
     };
 
 }
