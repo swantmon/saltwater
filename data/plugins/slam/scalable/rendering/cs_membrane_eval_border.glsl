a284aa2f7d3d5135a2907baa197b30f5a242898b
Kunert <kuno1234@gmx.de>
2019-03-14 14:56:13 +0100

    Compute color difference

--- data/plugins/slam/scalable/rendering/cs_membrane_eval_border.glsl
+++ data/plugins/slam/scalable/rendering/cs_membrane_eval_border.glsl
@@ -16,20 +16,28 @@ layout(std430, binding = 0) buffer BorderPatches
 layout (local_size_x = TILE_SIZE_2D, local_size_y = TILE_SIZE_2D, local_size_z = 1) in;
 void main()
 {
-    ivec2 ImageSize = imageSize(cs_Background);
+    ivec2 BackgroundSize = imageSize(cs_Background);
+    ivec2 DiminishedSize = imageSize(cs_Diminished);
     ivec2 MembraneSize = imageSize(cs_MembranePatches);
 
-    vec2 PatchPosition = g_PatchPositions[gl_WorkGroupID.x].xy * 16;
+    vec2 PatchPosition = g_PatchPositions[gl_WorkGroupID.x].xy * PATCH_SIZE;
     const ivec2 MembraneCoords = ivec2(PatchPosition + gl_LocalInvocationID.xy);
 
     imageStore(cs_MembranePatches, MembraneCoords, vec4(1.0f, 0.0f, 0.0f, 1.0f));
 
-    const ivec2 ImageCoords = ivec2(MembraneCoords / vec2(MembraneSize) * vec2(ImageSize));
-    const ivec2 InverseCoords = ivec2(ImageCoords.x, MembraneSize.y - ImageCoords.y);
+    const vec2 NormalizedCoords = MembraneCoords / vec2(MembraneSize);
 
-    vec4 Background = imageLoad(cs_Background, InverseCoords);
-    vec4 Diminished = imageLoad(cs_Diminished, ImageCoords);
-    
+    const ivec2 DiminishedCoords = ivec2(NormalizedCoords * vec2(DiminishedSize));
+    const ivec2 BackgroundCoords = ivec2(vec2(NormalizedCoords.x, 1.0f - NormalizedCoords.y) * vec2(BackgroundSize));
+
+    vec3 Diminished = imageLoad(cs_Diminished, DiminishedCoords).rgb;
+    vec3 Background = imageLoad(cs_Background, BackgroundCoords).rgb;
+
+    if(Diminished.x > 0.0f)
+    {
+        vec3 ColorDiff = Diminished - Background;
+        imageStore(cs_MembraneBorders, MembraneCoords, vec4(ColorDiff, 1.0f));
+    }
 }
 
 #endif //__INCLUDE_CS_YUV_TO_RGB_GLSL__
