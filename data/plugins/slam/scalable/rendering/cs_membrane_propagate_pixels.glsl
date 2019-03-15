e5e6e96fc83bb9fe98f1c81be403eb58d7968836
Kunert <kuno1234@gmx.de>
2019-03-15 16:24:08 +0100

    Fix coords

--- data/plugins/slam/scalable/rendering/cs_membrane_propagate_pixels.glsl
+++ data/plugins/slam/scalable/rendering/cs_membrane_propagate_pixels.glsl
@@ -17,15 +17,11 @@ layout(std430, binding = 0) buffer BorderPatches
 layout (local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
 void main()
 {
-    const ivec2 Coords = ivec2(gl_GlobalInvocationID.xy);
+    const ivec2 Coords = ivec2(gl_GlobalInvocationID.xy * PATCH_SIZE);
 
-    vec3 Color = imageLoad(cs_MembranePatches, Coords).rgb;
     vec3 GridColor = imageLoad(cs_MembraneBorders, Coords - (Coords % PATCH_SIZE)).rgb;
     
-    if (Color.r != 0.0f && Color.g != 0.0f && Color.b != 0.0f)
-    {
-        imageStore(cs_MembraneFinale, Coords, vec4(GridColor, 1.0f));
-    }
+    imageStore(cs_MembraneFinale, Coords / 16, vec4(GridColor, 1.0f));
 }
 
 #endif //__INCLUDE_CS_YUV_TO_RGB_GLSL__
