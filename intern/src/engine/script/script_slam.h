757286523b07b60d9247238fdbfa6a6c0ea11f60
Kunert <kuno1234@gmx.de>
2019-04-09 12:13:19 +0200

    Flip flag

--- intern/src/engine/script/script_slam.h
+++ intern/src/engine/script/script_slam.h
@@ -45,7 +45,7 @@ namespace Scpt
             SetActivateSelection = (FSetFlag)(Core::PluginManager::GetPluginFunction("SLAM", "SetActivateSelection"));
             EnableMouseControl = (FSetFlag)(Core::PluginManager::GetPluginFunction("SLAM", "EnableMouseControl"));
 
-            m_IsMouseControlEnabled = Core::CProgramParameters::GetInstance().Get("mr:slam:rendering:use_tracking_camera", false);
+            m_IsMouseControlEnabled = !Core::CProgramParameters::GetInstance().Get("mr:slam:rendering:use_tracking_camera", false);
         }
 
         // -----------------------------------------------------------------------------
