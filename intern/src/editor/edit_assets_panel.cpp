b60014f5fc154eb14a6a9b7ea514cbf6a9733ffa
Tobias Schwandt <t.schwandt@gmail.com>
2019-05-13 15:33:52 +0200

    Add first GUI from file dialog

--- intern/src/editor/edit_assets_panel.cpp
+++ intern/src/editor/edit_assets_panel.cpp
@@ -16,13 +16,43 @@
 
 #include "imgui/imgui.h"
 
+#include "base/base_defines.h"
+
+#ifdef PLATFORM_WINDOWS
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+
+#include <malloc.h>
+#include <stdio.h>
+#endif
+
+#include <algorithm>
+#include <chrono>
+#include <ctime>
+#include <filesystem>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
 namespace Edit
 {
 namespace GUI
 {
     CAssetsPanel::CAssetsPanel()
+        : m_Title("Assets")
+        , m_Regex(".*.[]?")
     {
+        m_CurrentPath = std::filesystem::current_path();
 
+        m_SelectedFiles.clear();
+
+        m_Sorter.m_SortModeName = SORT_DOWN;
+        m_Sorter.m_SortModeSize = UNSORTED;
+        m_Sorter.m_SortModeDate = UNSORTED;
+
+        ClearCache();
     }
 
     // -----------------------------------------------------------------------------
@@ -41,7 +71,193 @@ namespace GUI
 
         ImGui::Begin("Assets", &m_IsVisible);
 
-        
+        bool DoGoUp = false;
+
+        std::string GoDownString = "";
+
+        SFile* pSelectedFile = nullptr;
+
+        ImGui::SameLine();
+
+        ImGui::Text(m_CurrentPath.string().c_str());
+
+        ImGui::BeginChild("Directories", ImVec2(250, 350), true, ImGuiWindowFlags_HorizontalScrollbar);
+
+        if (ImGui::TreeNodeEx("Directories", ImGuiTreeNodeFlags_DefaultOpen))
+        {
+            if (ImGui::Selectable("..", false, 0, ImVec2(ImGui::GetWindowContentRegionWidth(), 0)))
+            {
+                DoGoUp = true;
+            }
+
+            for (auto& p : m_Directories)
+            {
+                if (ImGui::Selectable(p.c_str(), false, 0, ImVec2(ImGui::GetWindowContentRegionWidth(), 0)))
+                {
+                    GoDownString = p;
+                }
+            }
+
+            ImGui::TreePop();
+        }
+        ImGui::EndChild();
+
+        ImGui::SameLine();
+        {
+            std::string Header;
+
+            ImGui::BeginChild("Files", ImVec2(500, 350), true, ImGuiWindowFlags_HorizontalScrollbar);
+            ImGui::Columns(3);
+
+            switch (m_Sorter.m_SortModeName)
+            {
+            case UNSORTED:
+                Header = "  File";
+                break;
+            case SORT_DOWN:
+                Header = "v File";
+                break;
+            case SORT_UP:
+                Header = "^ File";
+                break;
+            }
+
+            if (ImGui::Selectable(Header.c_str()))
+            {
+                switch (m_Sorter.m_SortModeName)
+                {
+                case UNSORTED:
+                    m_Sorter.m_SortModeName = SORT_DOWN;
+                    break;
+                case SORT_DOWN:
+                    m_Sorter.m_SortModeName = SORT_UP;
+                    break;
+                case SORT_UP:
+                    m_Sorter.m_SortModeName = UNSORTED;
+                    break;
+                }
+
+                m_Sorter.m_SortModeSize = UNSORTED;
+                m_Sorter.m_SortModeDate = UNSORTED;
+
+                ClearCache();
+            }
+
+            ImGui::NextColumn();
+
+            switch (m_Sorter.m_SortModeSize)
+            {
+            case UNSORTED:
+                Header = "  Size";
+                break;
+            case SORT_DOWN:
+                Header = "v Size";
+                break;
+            case SORT_UP:
+                Header = "^ Size";
+                break;
+            }
+
+            if (ImGui::Selectable(Header.c_str()))
+            {
+                switch (m_Sorter.m_SortModeSize)
+                {
+                case UNSORTED:
+                    m_Sorter.m_SortModeSize = SORT_DOWN;
+                    break;
+                case SORT_DOWN:
+                    m_Sorter.m_SortModeSize = SORT_UP;
+                    break;
+                case SORT_UP:
+                    m_Sorter.m_SortModeSize = UNSORTED;
+                    break;
+                }
+                m_Sorter.m_SortModeName = UNSORTED;
+                m_Sorter.m_SortModeDate = UNSORTED;
+                ClearCache();
+            }
+            ImGui::NextColumn();
+
+            switch (m_Sorter.m_SortModeDate)
+            {
+            case UNSORTED:
+                Header = "  Date & Time";
+                break;
+            case SORT_DOWN:
+                Header = "v Date & Time";
+                break;
+            case SORT_UP:
+                Header = "^ Date & Time";
+                break;
+            }
+
+            if (ImGui::Selectable(Header.c_str()))
+            {
+                switch (m_Sorter.m_SortModeDate)
+                {
+                case UNSORTED:
+                    m_Sorter.m_SortModeDate = SORT_DOWN;
+                    break;
+                case SORT_DOWN:
+                    m_Sorter.m_SortModeDate = SORT_UP;
+                    break;
+                case SORT_UP:
+                    m_Sorter.m_SortModeDate = UNSORTED;
+                    break;
+                }
+                m_Sorter.m_SortModeName = UNSORTED;
+                m_Sorter.m_SortModeSize = UNSORTED;
+
+                ClearCache();
+            }
+
+            ImGui::NextColumn();
+            ImGui::Separator();
+
+            for (auto& rCurrentFile : m_Files)
+            {
+                std::string label = std::string("##") + rCurrentFile.m_Filename;
+
+                if (ImGui::Selectable(label.c_str(), rCurrentFile.m_Selected, ImGuiSelectableFlags_SpanAllColumns))
+                {
+                    for (auto& rFile : m_Files) rFile.m_Selected = false;
+
+                    rCurrentFile.m_Selected = true;
+                }
+
+                ImGui::SameLine();
+                ImGui::Text(rCurrentFile.m_Filename.c_str());
+                ImGui::NextColumn();
+                ImGui::Text(std::to_string(rCurrentFile.m_Size).c_str());
+                ImGui::NextColumn();
+                ImGui::Text(rCurrentFile.m_DateTime.c_str());
+                ImGui::NextColumn();
+
+                if (rCurrentFile.m_Selected) pSelectedFile = &rCurrentFile;
+            }
+
+            ImGui::EndChild();
+        }
+
+        std::string SelectedFileString;
+        bool gotSelected = pSelectedFile;
+
+        SelectedFileString = (m_CurrentPath / std::filesystem::path(pSelectedFile ? pSelectedFile->m_Filename : "...")).string();
+
+        std::replace(SelectedFileString.begin(), SelectedFileString.end(), '\\', '/');
+      
+        if (DoGoUp)
+        {
+            m_CurrentPath = m_CurrentPath.parent_path();
+
+            ClearCache();
+        }
+        else if (!GoDownString.empty())
+        {
+            m_CurrentPath = m_CurrentPath / GoDownString;
+
+            ClearCache();
+        }
 
         ImGui::End();
     }
@@ -50,7 +266,110 @@ namespace GUI
 
     const char* CAssetsPanel::GetName()
     {
-        return "Assets";
+        return m_Title.c_str();
+    }
+
+    // -----------------------------------------------------------------------------
+
+    void CAssetsPanel::FillRoots()
+    {
+        DWORD Drives = GetLogicalDrives();
+
+        for (TCHAR CurrentDrive = 'A'; CurrentDrive <= 'Z'; CurrentDrive++, Drives >>= 1)
+        {
+            if (!(Drives & 1)) continue;
+
+            BOOL success = FALSE;
+            TCHAR rootPath[4];
+            TCHAR volumeName[MAX_PATH + 1];
+
+            rootPath[0] = CurrentDrive;
+            rootPath[1] = ':';
+            rootPath[2] = '\\';
+            rootPath[3] = '\0';
+
+            success = GetVolumeInformation(rootPath, volumeName, MAX_PATH + 1, nullptr, nullptr, nullptr, nullptr, 0);
+
+            if (!success) continue;
+
+#ifdef UNICODE
+            int needed;
+            LPSTR str;
+
+            needed = WideCharToMultiByte(CP_UTF8, 0, rootPath, -1, nullptr, 0, nullptr, nullptr);
+            if (needed <= 0) continue;
+            str = (LPSTR)_malloca(needed);
+            WideCharToMultiByte(CP_UTF8, 0, rootPath, -1, str, needed, nullptr, nullptr);
+            std::string root = str;
+            _freea(str);
+
+            needed = WideCharToMultiByte(CP_UTF8, 0, volumeName, -1, nullptr, 0, nullptr, nullptr);
+            if (needed <= 0) continue;
+            str = (LPSTR)_malloca(needed);
+            WideCharToMultiByte(CP_UTF8, 0, volumeName, -1, str, needed, nullptr, nullptr);
+            std::string label = root + " (" + str + ")";
+            _freea(str);
+#else
+            std::string root = rootName;
+            std::string label = root + " (" + volumeName + ")";
+#endif
+            m_Roots.push_back({ root, label });
+        }
+    }
+
+    // -----------------------------------------------------------------------------
+
+    void CAssetsPanel::ClearCache()
+    {
+        m_Roots.clear();
+        m_Directories.clear();
+        m_Files.clear();
+
+        FillRoots();
+
+        m_SpaceInfo = std::filesystem::space(m_CurrentPath);
+
+        for (auto& rCurrentItemInPath : std::filesystem::directory_iterator(m_CurrentPath))
+        {
+            if (rCurrentItemInPath.is_directory())
+            {
+                m_Directories.push_back(rCurrentItemInPath.path().filename().string());
+            }
+            else
+            {
+                try
+                {
+                    auto status = rCurrentItemInPath.status();
+                    auto lastWrite = std::filesystem::last_write_time(rCurrentItemInPath);
+                    auto timeSinceEpoch = lastWrite.time_since_epoch();
+                    auto count = timeSinceEpoch.count();
+
+                    std::time_t dateTime = count / 100000000;
+
+                    const std::tm* converted = std::localtime(&dateTime);
+
+                    std::ostringstream formatted;
+
+                    formatted << std::put_time(converted, "%c");
+
+                    if (std::regex_match(rCurrentItemInPath.path().filename().string(), m_Regex))
+                    {
+                        m_Files.push_back({ rCurrentItemInPath.path().filename().string(), std::filesystem::file_size(rCurrentItemInPath), formatted.str(), dateTime });
+                    }
+
+                }
+                catch (...)
+                {
+                }
+            }
+        }
+
+        if (m_Sorter.m_SortModeName != UNSORTED || m_Sorter.m_SortModeSize != UNSORTED || m_Sorter.m_SortModeDate != UNSORTED)
+        {
+            std::sort(m_Files.begin(), m_Files.end(), m_Sorter);
+        }
+
+        std::sort(m_Directories.begin(), m_Directories.end());
     }
 } // namespace GUI
 } // namespace Edit
--- intern/src/editor/edit_assets_panel.h
+++ intern/src/editor/edit_assets_panel.h
@@ -7,8 +7,10 @@
 
 #include "engine/data/data_entity.h"
 
+#include <filesystem>
+#include <regex>
+#include <string>
 #include <vector>
-#include <map>
 
 namespace Edit
 {
@@ -29,6 +31,80 @@ namespace GUI
 
         const char* GetName() override;
 
+    private:
+
+        enum ESortMode
+        {
+            UNSORTED,
+            SORT_DOWN,
+            SORT_UP
+        };
+
+    private:
+
+        struct SRoot
+        {
+            std::string m_Root;
+            std::string m_Label;
+        };
+
+        struct SFile
+        {
+            std::string m_Filename;
+            std::uintmax_t m_Size;
+            std::string m_DateTime;
+            std::time_t m_DateTimeTimeT;
+            bool m_Selected;
+        };
+
+        struct SSorter
+        {
+            bool operator()(const SFile& a, const SFile& b) const
+            {
+                switch (m_SortModeName)
+                {
+                case SORT_DOWN: return a.m_Filename < b.m_Filename;
+                case SORT_UP:   return a.m_Filename > b.m_Filename;
+                }
+
+                switch (m_SortModeSize)
+                {
+                case SORT_DOWN: return a.m_Size < b.m_Size;
+                case SORT_UP:   return a.m_Size > b.m_Size;
+                }
+
+                switch (m_SortModeDate)
+                {
+                case SORT_DOWN: return a.m_DateTimeTimeT < b.m_DateTimeTimeT;
+                case SORT_UP:   return a.m_DateTimeTimeT > b.m_DateTimeTimeT;
+                }
+
+                return false;
+            }
+            ESortMode m_SortModeName = SORT_DOWN;
+            ESortMode m_SortModeSize = UNSORTED;
+            ESortMode m_SortModeDate = UNSORTED;
+        };
+
+    private:
+
+        std::string m_Title;
+        std::regex m_Regex;
+
+        std::vector<SRoot> m_Roots;
+        std::vector<std::string> m_Directories;
+        std::vector<SFile> m_Files;
+        std::vector<std::string> m_SelectedFiles;
+
+        SSorter m_Sorter;
+
+        std::filesystem::path m_CurrentPath;
+        std::filesystem::space_info m_SpaceInfo;
+
+    private:
+
+        void FillRoots();
+        void ClearCache();
     };
 } // namespace GUI
 } // namespace Edit
