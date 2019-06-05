251550ac52b6aaa18c97e57dfc6bb9c32a5b5d83
Kunert <kuno1234@gmx.de>
2019-06-05 14:33:41 +0200

    Remove ocean project

--- intern/src/plugin/pixmix/pm_plugin_interface.cpp
+++ intern/src/plugin/pixmix/pm_plugin_interface.cpp
@@ -5,9 +5,66 @@
 #include "engine/core/core_console.h"
 #include "engine/engine.h"
 #include "plugin/pixmix/pm_plugin_interface.h"
-#include "plugin/pixmix/PixMix/PixMix.h"
 
+#define OCEAN_PIXMIX
+
+#ifndef OCEAN_PIXMIX
+
+#if _DEBUG
+
+#pragma comment(lib, "opencv_world410d.lib")
+
+#else
+
+#pragma comment(lib, "opencv_world410.lib")
+
+#endif
+
+#include "plugin/pixmix/PixMix/PixMix.h"
 #include "opencv2/opencv.hpp"
+
+#else
+
+#if _DEBUG
+
+#pragma comment(lib, "OceanBaseD.lib")
+#pragma comment(lib, "OceanCVD.lib")
+#pragma comment(lib, "OceanCVDetectorD.lib")
+#pragma comment(lib, "OceanCVSegmentationD.lib")
+#pragma comment(lib, "OceanCVSynthesisD.lib")
+#pragma comment(lib, "OceanGeometryD.lib")
+#pragma comment(lib, "OceanIOD.lib")
+#pragma comment(lib, "OceanMathD.lib")
+#pragma comment(lib, "OceanMediaD.lib")
+#pragma comment(lib, "OceanNetworkD.lib")
+#pragma comment(lib, "OceanPlatformWinD.lib")
+
+#else
+
+#pragma comment(lib, "OceanBase.lib")
+#pragma comment(lib, "OceanCV.lib")
+#pragma comment(lib, "OceanCVDetector.lib")
+#pragma comment(lib, "OceanCVSegmentation.lib")
+#pragma comment(lib, "OceanCVSynthesis.lib")
+#pragma comment(lib, "OceanGeometry.lib")
+#pragma comment(lib, "OceanIO.lib")
+#pragma comment(lib, "OceanMath.lib")
+#pragma comment(lib, "OceanMedia.lib")
+#pragma comment(lib, "OceanNetwork.lib")
+#pragma comment(lib, "OceanPlatformWin.lib")
+
+#endif
+
+#pragma warning(push)
+#pragma warning(disable : 4100 4245 5033)
+
+#define _WINDOWS
+#undef _DLL
+#include "ocean/cv/synthesis/SynthesisOneFramePixel.h"
+
+#pragma warning(pop)
+
+#endif
 
 #include <vector>
 
@@ -56,6 +113,7 @@ namespace PM
 
     // -----------------------------------------------------------------------------
 
+#ifndef OCEAN_PIXMIX
     void CPluginInterface::Inpaint(const glm::ivec2& _Resolution, const std::vector<glm::u8vec4>& _SourceImage, std::vector<glm::u8vec4>& _DestinationImage)
     {
         assert(_Resolution.x > 0 && _Resolution.y > 0);
@@ -123,6 +181,50 @@ namespace PM
 
         std::memcpy(_DestinationImage.data(), Dest4.data, _DestinationImage.size() * sizeof(_DestinationImage[0]));
     }
+
+#else
+
+    void CPluginInterface::Inpaint(const glm::ivec2& _Resolution, const std::vector<glm::u8vec4>& _SourceImage, std::vector<glm::u8vec4>& _DestinationImage)
+    {
+        assert(_Resolution.x > 0 && _Resolution.y > 0);
+        assert(_SourceImage.size() == _DestinationImage.size());
+
+        std::vector<glm::u8vec3> Image(_Resolution.x * _Resolution.y);
+        std::vector<unsigned char> Mask(_Resolution.x * _Resolution.y);
+
+        for (int i = 0; i < _Resolution.x * _Resolution.y; ++i)
+        {
+            glm::u8vec4 Pixel = _SourceImage[i];
+            Image[i] = glm::u8vec3(Pixel.r, Pixel.g, Pixel.b);
+            Mask[i] = Pixel.r == 255 && Pixel.g == 255 && Pixel.b == 255 ? 0x00 : 0xFF;
+        }
+
+        Ocean::CV::Synthesis::SynthesisOneFramePixel PixMix;
+        Ocean::RandomGenerator Generator;
+
+        Ocean::FrameType InputType(_Resolution.x, _Resolution.y, Ocean::FrameType::FORMAT_RGB24, Ocean::FrameType::PixelOrigin::ORIGIN_UPPER_LEFT);
+        Ocean::Frame Input(InputType, &Image[0].r, true);
+
+        Ocean::FrameType MaskType(_Resolution.x, _Resolution.y, Ocean::FrameType::FORMAT_Y8, Ocean::FrameType::PixelOrigin::ORIGIN_UPPER_LEFT);
+        Ocean::Frame MaskF(MaskType, Mask.data(), true);
+
+        PixMix.initialize(Input, MaskF);
+
+        PixMix.applyInpainting(Generator, Ocean::CV::Synthesis::SynthesisOneFramePixel::IT_PATCH_REGION_2);
+
+        PixMix.createInpaintingResult(Input);
+
+        _DestinationImage.resize(_Resolution.x * _Resolution.y);
+
+        const auto pOutputPixels = reinterpret_cast<glm::u8vec3*>(Input.data());
+
+        for (int i = 0; i < _Resolution.x * _Resolution.y; ++i)
+        {
+            _DestinationImage[i] = glm::u8vec4(pOutputPixels[i], 255);
+        }
+    }
+
+#endif
 } // namespace PM
 
 extern "C" CORE_PLUGIN_API_EXPORT void Inpaint(const glm::ivec2& _Resolution, const std::vector<glm::u8vec4>& _SourceImage, std::vector<glm::u8vec4>& _DestinationImage)
--- intern/src/plugin/pixmix_ocean/pmo_precompiled.cpp
+++
@@ -1,2 +0,0 @@
-
-#include "plugin/pixmix_ocean/pmo_precompiled.h"
