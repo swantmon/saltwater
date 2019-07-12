d11bc174d111015617e8f40d764cb43c8ca4bf31
Tobias Schwandt <t.schwandt@gmail.com>
2019-07-12 10:13:33 +0200

    Fix bug w/ adding components

--- intern/src/editor/edit_data_ssr_component.h
+++ intern/src/editor/edit_data_ssr_component.h
@@ -30,7 +30,7 @@ namespace Dt
 
         // -----------------------------------------------------------------------------
 
-        void OnNewComponent(Dt::CEntity::BID _ID)
+        static void OnNewComponent(Dt::CEntity::BID _ID)
         {
             Dt::CEntity* pCurrentEntity = Dt::CEntityManager::GetInstance().GetEntityByID(_ID);
 
