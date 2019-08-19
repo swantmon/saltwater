83cb6b555673cf0772a7eec6c7df7c7238888048
Christian <kuno1234@gmx.de>
2019-08-19 14:38:12 +0200

    Add default parameters and new functions for 2d textures

--- intern/src/engine/graphic/gfx_texture_manager.h
+++ intern/src/engine/graphic/gfx_texture_manager.h
@@ -70,7 +70,8 @@ namespace TextureManager
     ENGINE_API void CopyToTextureArray2D(CTexturePtr _TextureArrayPtr, unsigned int _IndexOfSlice, CTexturePtr _TexturePtr, bool _UpdateMipLevels = false);
 
     ENGINE_API void CopyTexture(CTexturePtr _SourceTexturePtr, CTexturePtr _TargetTexturePtr);
-    ENGINE_API void CopyTexture(CTexturePtr _SourceTexturePtr, CTexturePtr _TargetTexturePtr, const glm::ivec3& _rSourceMin, const glm::ivec3& _rTargetMin, const glm::ivec3& _rSize, int _SourceLevel, int _TargetLevel);
+    ENGINE_API void CopyTexture(CTexturePtr _SourceTexturePtr, CTexturePtr _TargetTexturePtr, const glm::ivec2& _rSourceMin, const glm::ivec2& _rTargetMin, const glm::ivec2& _rSize, int _SourceLevel = 0, int _TargetLevel = 0);
+    ENGINE_API void CopyTexture(CTexturePtr _SourceTexturePtr, CTexturePtr _TargetTexturePtr, const glm::ivec3& _rSourceMin, const glm::ivec3& _rTargetMin, const glm::ivec3& _rSize, int _SourceLevel = 0, int _TargetLevel = 0);
 
     ENGINE_API void CopyActiveTargetSetToTexture(CTexturePtr _TexturePtr, const Base::AABB2UInt& _rTargetRect);
 
