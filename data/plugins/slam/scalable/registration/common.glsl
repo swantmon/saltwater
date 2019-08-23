3a41a2d62073317bbe153f7c62a58c23995c0e6f
chkunert <kuno1234@gmx.de>
2019-08-23 16:42:41 +0200

    Sum values

--- data/plugins/slam/scalable/registration/common.glsl
+++ data/plugins/slam/scalable/registration/common.glsl
@@ -6,6 +6,8 @@ layout(std430, binding = 0) buffer Buffer
     vec4 g_Sum[TILE_COUNT_X * TILE_COUNT_Y];
 };
 
+#define WORKGROUP_SIZE ((TILE_SIZE2D) * (TILE_SIZE2D)
+
 layout(binding = 0, rgba8) uniform image2D cs_Target;
 layout(binding = 1, rgba8) uniform image2D cs_Source;
 layout(binding = 2, rgba32f) uniform image2D cs_SSD;
--- data/plugins/slam/scalable/registration/cs_sum_tiles.glsl
+++ data/plugins/slam/scalable/registration/cs_sum_tiles.glsl
@@ -3,10 +3,34 @@
 
 #include "../../plugins/slam/scalable/registration/common.glsl"
 
+shared vec4 g_SharedData[TILE_SIZE2D * TILE_SIZE2D];
+
+void reduce()
+{
+    for (int i = TILE_SIZE2D * TILE_SIZE2D; i >= 1; i /= 2)
+    {
+        if (gl_LocalInvocationIndex < i / 2)
+        {
+            g_SharedData[gl_LocalInvocationIndex] += g_SharedData[gl_LocalInvocationIndex + i / 2];
+        }
+        barrier();
+    }
+}
+
 layout (local_size_x = TILE_SIZE2D, local_size_y = TILE_SIZE2D, local_size_z = 1) in;
 void main()
 {
     ivec2 Coords = ivec2(gl_GlobalInvocationID.xy);
+
+    g_SharedData[gl_LocalInvocationIndex] = imageLoad(cs_Gradient, Coords);
+    barrier();
+
+    reduce();
+
+    if (gl_LocalInvocationIndex == 0)
+    {
+        g_Sum[gl_WorkGroupID.x * TILE_SIZE2D + gl_WorkGroupID.y] = g_SharedData[0];
+    }
 }
 
 #endif //__INCLUDE_CS_SUM_TILES_GLSL__
