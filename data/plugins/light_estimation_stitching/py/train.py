ae2d9880fc002788363c18fdca6f3d405598fc47
Tobias Schwandt <t.schwandt@gmail.com>
2020-04-17 12:29:22 +0200

    Add perlin noise for mask instead of rectangles

--- data/plugins/light_estimation_stitching/py/train.py
+++ data/plugins/light_estimation_stitching/py/train.py
@@ -54,6 +54,41 @@ os.makedirs(opt.path_to_savepoint, exist_ok=True)
 # -----------------------------------------------------------------------------
 # Dataset
 # -----------------------------------------------------------------------------
+def generate_perlin_noise_2d(shape, res):
+    def f(t):
+        return 6*t**5 - 15*t**4 + 10*t**3
+    
+    delta = (res[0] / shape[0], res[1] / shape[1])
+    d = (shape[0] // res[0], shape[1] // res[1])
+    grid = np.mgrid[0:res[0]:delta[0],0:res[1]:delta[1]].transpose(1, 2, 0) % 1
+    # Gradients
+    angles = 2*np.pi*np.random.rand(res[0]+1, res[1]+1)
+    gradients = np.dstack((np.cos(angles), np.sin(angles)))
+    g00 = gradients[0:-1,0:-1].repeat(d[0], 0).repeat(d[1], 1)
+    g10 = gradients[1:  ,0:-1].repeat(d[0], 0).repeat(d[1], 1)
+    g01 = gradients[0:-1,1:  ].repeat(d[0], 0).repeat(d[1], 1)
+    g11 = gradients[1:  ,1:  ].repeat(d[0], 0).repeat(d[1], 1)
+    # Ramps
+    n00 = np.sum(np.dstack((grid[:,:,0]  , grid[:,:,1]  )) * g00, 2)
+    n10 = np.sum(np.dstack((grid[:,:,0]-1, grid[:,:,1]  )) * g10, 2)
+    n01 = np.sum(np.dstack((grid[:,:,0]  , grid[:,:,1]-1)) * g01, 2)
+    n11 = np.sum(np.dstack((grid[:,:,0]-1, grid[:,:,1]-1)) * g11, 2)
+    # Interpolation
+    t = f(grid)
+    n0 = n00*(1-t[:,:,0]) + t[:,:,0]*n10
+    n1 = n01*(1-t[:,:,0]) + t[:,:,0]*n11
+    return np.sqrt(2)*((1-t[:,:,1])*n0 + t[:,:,1]*n1)
+        
+def generate_fractal_noise_2d(shape, res, octaves=1, persistence=0.5):
+    noise = np.zeros(shape)
+    frequency = 1
+    amplitude = 1
+    for _ in range(octaves):
+        noise += amplitude * generate_perlin_noise_2d(shape, (frequency*res[0], frequency*res[1]))
+        frequency *= 2
+        amplitude *= persistence
+    return noise
+
 class ImageDataset(Dataset):
     def __init__(self, root, transforms_=None, mode='train'):
         self.transform = transforms.Compose(transforms_)
@@ -63,31 +98,21 @@ class ImageDataset(Dataset):
 
     # -----------------------------------------------------------------------------
 
+    def generateMaskTensor(self):
+        noise = generate_perlin_noise_2d((128, 256), (2, 2))
+        noise = noise * 2.0 + 0.6
+        noise = 1.0 - np.clip(noise, 0, 1)
+        return torch.Tensor(noise)
+
+    # -----------------------------------------------------------------------------
+
     def apply_random_mask(self, img):
         masked_part = img.clone()
         masked_img = img.clone()
 
-        y1 = int(opt.img_size_h * 0.0)
-        y2 = int(opt.img_size_h * opt.mask_ground_and_sky)
-        x1 = int(0)
-        x2 = int(opt.img_size_w)
+        noise = self.generateMaskTensor()
 
-        masked_img[:, y1:y2, x1:x2] = 1
-
-        y1 = int(opt.img_size_h * (1.0 - opt.mask_ground_and_sky * 0.4))
-        y2 = int(opt.img_size_h * 1.0)
-        x1 = int(0)
-        x2 = int(opt.img_size_w)
-
-        masked_img[:, y1:y2, x1:x2] = 1
-        
-        for _ in range(random.randrange(1, opt.number_of_masks + 1)):
-            y1 = np.random.randint(0, opt.img_size_h - opt.mask_size)
-            x1 = np.random.randint(0, opt.img_size_w - int(opt.mask_size * 1.5))
-
-            y2, x2 = y1 + opt.mask_size, x1 + int(opt.mask_size * 1.5)
-
-            masked_img[:, y1:y2, x1:x2] = 1
+        masked_img = torch.ones(img.shape) * (1.0 - noise) + masked_img * noise
 
         return masked_img, masked_part
 
@@ -107,30 +132,10 @@ class ImageDataset(Dataset):
         masked_part = torch.zeros(img.shape)
         masked_img = img.clone()
 
-        y1 = int(opt.img_size_h * 0.0)
-        y2 = int(opt.img_size_h * opt.mask_ground_and_sky)
-        x1 = int(0)
-        x2 = int(opt.img_size_w)
+        noise = self.generateMaskTensor()
 
-        masked_part[:, y1:y2, x1:x2] = 1
-        masked_img[:, y1:y2, x1:x2] = 1
-
-        y1 = int(opt.img_size_h * (1.0 - opt.mask_ground_and_sky * 0.4))
-        y2 = int(opt.img_size_h * 1.0)
-        x1 = int(0)
-        x2 = int(opt.img_size_w)
-
-        masked_part[:, y1:y2, x1:x2] = 1
-        masked_img[:, y1:y2, x1:x2] = 1
-        
-        for _ in range(random.randrange(1, opt.number_of_masks + 1)):
-            y1 = np.random.randint(0, opt.img_size_h - opt.mask_size)
-            x1 = np.random.randint(0, opt.img_size_w - int(opt.mask_size * 1.5))
-
-            y2, x2 = y1 + opt.mask_size, x1 + int(opt.mask_size * 1.5)
-
-            masked_part[:, y1:y2, x1:x2] = 1
-            masked_img[:, y1:y2, x1:x2] = 1
+        masked_img = torch.ones(img.shape) * (1.0 - noise) + masked_img * noise
+        masked_part = torch.ones(img.shape) * (1.0 - noise) + masked_part * noise
 
         return masked_img, masked_part
 
